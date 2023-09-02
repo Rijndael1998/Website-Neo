f158edfbf2b2be5dcf63e4f0857d73c2855a799e
Lukasz Baldyga <rijndael@baldy.ga>
2023-09-02 18:05:10 +0100

    added very basic page

--- src/app/fun/page.tsx
+++ src/app/fun/page.tsx
@@ -1,13 +1,41 @@
 export default function Fun() {
     return <>
         <h1>Portfolio</h1>
-        {
-            // Rchan
-            // Password Manger
-            // Lagrange
-            // Sudoku Solver
-            // Drawing Project
-            // A*
-        }
+
+        <div>
+            <h2>Rchan</h2>
+            <p>{`Rchan is project I've worked on that bridges the gap between regular people and AI. It's a simple discord bot that you can talk to with your friends. Think of it as a group chat with ChatGPT It also supports voice calls in groups, as well as taking on different personalities.`}</p>
+        </div>
+
+        <div>
+            <h2>Internet Nomad Password Manager</h2>
+            <p>The Internet Nomad password manager is built upon research into encryption types. It focuses on the fact that a person maybe have one or more identities online. It also offers unconventional recovery methods to allow users to recover from catastrophe.</p>
+        </div>
+
+        <div>
+            <h2>Langrage Demo</h2>
+            <p></p>
+        </div>
+
+        <div>
+            <h2>Sudoku Solver</h2>
+            <p>
+
+            </p>
+        </div>
+
+        <div>
+            <h2>Drawing Project</h2>
+            <p>
+
+            </p>
+        </div>
+
+        <div>
+            <h2>A*</h2>
+            <p>
+
+            </p>
+        </div>
     </>
 }
