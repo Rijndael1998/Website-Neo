e2d386cc492bae8522d22b5fbb206e9139259696
Lukasz Baldyga <rijndael@baldy.ga>
2023-09-08 00:49:16 +0100

    Move stylings

--- src/app/fun/a-star/page.tsx
+++ src/app/fun/a-star/page.tsx
@@ -3,7 +3,14 @@
 import { useEffect, useState } from "react";
 import { GridState } from "../../../components/algorithms/grid/gridState"
 import Grid from "../../../components/algorithms/grid/_grid";
-import { AStarStates, AStarStyleMap } from "@/components/algorithms/a*/_a*";
+import { AStarStates } from "@/components/algorithms/a*/_a*";
+import styles from "./a*.module.scss";
+
+export const AStarStyleMap: Map<AStarStates,string> = new Map();
+AStarStyleMap.set(AStarStates.Node, styles.Node);
+AStarStyleMap.set(AStarStates.Wall, styles.Wall);
+AStarStyleMap.set(AStarStates.Start, styles.Start);
+AStarStyleMap.set(AStarStates.End, styles.End);
 
 
 export default function A_Star() {
@@ -25,6 +32,11 @@ export default function A_Star() {
         <h1>
             A* Algorithm
         </h1>
+        <div>
+            <div>Select start</div>
+            <div>Select end</div>
+            <div>Select walls</div>
+        </div>
         <Grid state={state} callback={callback} />
     </>
 }
--- src/components/algorithms/a*/_a*.ts
+++ src/components/algorithms/a*/_a*.ts
@@ -1,5 +1,4 @@
 import { GridState } from "../grid/gridState";
-import styles from "./a*.module.scss";
 
 export enum AStarStates {
     Node,
@@ -7,12 +6,6 @@ export enum AStarStates {
     Start,
     End,
 }
-
-export const AStarStyleMap: Map<AStarStates,string> = new Map();
-AStarStyleMap.set(AStarStates.Node, styles.Node);
-AStarStyleMap.set(AStarStates.Wall, styles.Wall);
-AStarStyleMap.set(AStarStates.Start, styles.Start);
-AStarStyleMap.set(AStarStates.End, styles.End);
 
 export default class AStar {
     constructor(public state: GridState<AStarStates>) {
