c6105a2e65656cfe2ee8d5e176863e8e828d4658
Lukasz Baldyga <rijndael@baldy.ga>
2023-09-24 12:07:29 +0100

    Added solver

--- src/app/fun/sudoku/page.tsx
+++ src/app/fun/sudoku/page.tsx
@@ -5,6 +5,8 @@ import GenericButton from "@/components/input/genericButton/_genericButton";
 import { Duplicate2DArray } from "@/components/util";
 import { useState } from "react";
 import styles from "./sudoku.module.scss";
+import { EasyGrid, HardGrid } from "@/components/algorithms/sudoku/testGrids";
+import { backtracking } from "@/components/algorithms/sudoku/sudokuWorker";
 
 function EmptyGridGen() {
     const grid: Array<Array<number>> = [];
@@ -27,16 +29,37 @@ export default function Sudoku() {
         setGrid(newGrid);
     }
 
-    console.log(grid);
+    const puzzleHandler = (type: "easy" | "hard") => {
+        switch(type) {
+            case "easy":
+                setGrid(Duplicate2DArray(EasyGrid));
+                return;
+
+            case "hard":
+                setGrid(Duplicate2DArray(HardGrid));
+                return;
+        }
+    }
+
+    const solve = () => {
+        const result = backtracking(Duplicate2DArray(grid), 0);
+
+        if(result === false) {
+            console.log("no result");
+            return;
+        }
+
+        setGrid(result);
+    }
 
     return <>
         <h1>Sudoku Solver</h1>
         <div className={styles.controlWrapper}>
             <div className={styles.buttonWrapper}>
-                <GenericButton className={styles.button}>Easy Puzzle</GenericButton>
-                <GenericButton className={styles.button}>Hard Puzzle</GenericButton>
-                <GenericButton className={styles.button}>Solve</GenericButton>
-
+                <GenericButton className={styles.button} onClick={() => puzzleHandler("easy")}>Easy Puzzle</GenericButton>
+                <GenericButton className={styles.button} onClick={() => puzzleHandler("hard")}>Hard Puzzle</GenericButton>
+                <GenericButton className={styles.button} onClick={() => solve()}>Solve</GenericButton>
+                <GenericButton className={styles.button} onClick={() => setGrid(EmptyGridGen())}>Clear</GenericButton>
             </div>
             <SudokuGrid grid={grid} callback={(x, y, v) => { callback(x, y, v) }} />
         </div>
