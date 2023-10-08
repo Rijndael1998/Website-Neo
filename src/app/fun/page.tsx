6ee64d59a106216edaee73a7843cc977c17b3aa5
Lukasz Baldyga <rijndael@baldy.ga>
2023-10-09 00:13:25 +0100

    added breakpoints

--- src/app/fun/page.tsx
+++ src/app/fun/page.tsx
@@ -9,22 +9,24 @@ import styles from "./page.module.scss";
 */
 export default function Fun() {
     return <>
-        <h1>Portfolio</h1>
+        <div className={styles.contentWrapper}>
+            <h1>Portfolio</h1>
 
-        <h2>Previous Work</h2>
+            <h2>Previous Work</h2>
 
-        <div className={styles.content}>
-            {irlContent.map(
-                (portfolio) =>
-                    <Portfolio key={portfolio.title} portfolio={portfolio} />)}
-        </div>
+            <div className={styles.content}>
+                {irlContent.map(
+                    (portfolio) =>
+                        <Portfolio key={portfolio.title} portfolio={portfolio} />)}
+            </div>
 
-        <h2>Personal Projects</h2>
+            <h2>Personal Projects</h2>
 
-        <div className={styles.content}>
-            {content.map(
-                (portfolio) =>
-                    <Portfolio key={portfolio.title} portfolio={portfolio} />)}
+            <div className={styles.content}>
+                {content.map(
+                    (portfolio) =>
+                        <Portfolio key={portfolio.title} portfolio={portfolio} />)}
+            </div>
         </div>
     </>
 }
