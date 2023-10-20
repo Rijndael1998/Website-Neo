eee539502e2e5b0e196d1cff1b4288857b3b0d49
Lukasz Baldyga <rijndael@baldy.ga>
2023-10-20 20:11:53 +0100

    Fixed

--- src/components/app/footer/_footer.tsx
+++ src/components/app/footer/_footer.tsx
@@ -3,6 +3,8 @@
 import { useEffect, useState } from "react";
 import styles from "./footer.module.scss";
 import { usePathname } from 'next/navigation';
+import GalleryImage from "@/components/imageGallery/galleryImage";
+import Image from "next/image";
 
 export default function Footer() {
     // const pathname = usePathname();
@@ -15,6 +17,7 @@ export default function Footer() {
 
     return <footer className={styles.footer}>
         <div>
+            <Image src="/favico.png" width={50} height={50} alt="logo"/>
             {/* <p>{path}</p> */}
             <p>
                 Lukasz Baldyga
