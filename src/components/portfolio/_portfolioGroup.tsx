51f13c22a199e9ec213bfb1b1a4747f607d84d01
Lukasz Baldyga <rijndael@baldy.ga>
2024-01-21 20:01:47 +0000

    feat: Add volunteering section to portfolio page
    This commit adds a new section called "Volunteering" to the portfolio page. The section displays my volunteer experiences and initiatives. It also includes a title and subtitle for each portfolio group, providing more context for the displayed content.

--- src/components/portfolio/_portfolioGroup.tsx
+++ src/components/portfolio/_portfolioGroup.tsx
@@ -2,12 +2,16 @@ import styles from "./portfolio.module.scss";
 import PortfolioItem from "./_portfolioItem";
 import { PortfolioViewArray } from "@/content/portfolio/Portfolio";
 
-export default function PortfolioGroup({ items }: { items: PortfolioViewArray }) {
-    return <div className={styles.content}>
-        {
-            items.map(
-                (portfolio) =>
-                    <PortfolioItem key={portfolio.title} portfolio={portfolio} />)
-        }
-    </div>
+export default function PortfolioGroup({ items, title, subtitle }: { items: PortfolioViewArray, title: string, subtitle: string }) {
+    return <>
+        <h2>{title}</h2>
+        <p>{subtitle}</p>
+        <div className={styles.content}>
+            {
+                items.map(
+                    (portfolio) =>
+                        <PortfolioItem key={portfolio.title} portfolio={portfolio} />)
+            }
+        </div>
+    </>
 }
