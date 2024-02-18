21e931b265945447580188ff9f029aab4b0f38ab
Lukasz Baldyga <rijndael@baldy.ga>
2024-02-18 20:54:16 +0000

    Removed several files and cleaned up code
    Deleted multiple binary files and a HTML file. This is part of an effort to clean up the repository by removing unnecessary or outdated files.

--- public/fun/hobbies/drawingProgram/html/search/variables_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['brush_375',['brush',['../class_advanced___software___engineering_1_1_drawer.html#a9c14923dd98b1014dc4a1455d384b717',1,'Advanced_Software_Engineering::Drawer']]]
-];
