e19bb830e48dfe5073aa95db096972482c648fa8
Lukasz Baldyga <rijndael@baldy.ga>
2024-04-16 19:13:19 +0100

    Refactor Decider class and related functions
    The Decider class has been refactored to improve code readability and maintainability. The changes include:
    - Introduction of new types: `QuestionEntryType` and `QuestionYesNoType`.
    - Refactoring the constructor of the `Question` class to use these new types.
    - Addition of a scaling method in the `Question` class.
    - Modification of how questions are defined, now using an object structure for clarity.
    - Adjustment calculations have been moved into a separate constant for better encapsulation.

--- src/content/portfolio/decider/Decider.ts
+++ src/content/portfolio/decider/Decider.ts
@@ -1,60 +1,232 @@
 export const deciderP1 = ``;
 
+export type QuestionEntryType = [
+    question: string,
+    description?: string,
+];
+
+export type QuestionYesNoType = {
+    // yes
+    y: number;
+
+    // no
+    n: number;
+
+    // invert
+    inv: boolean;
+};
+
 export class Question {
-    public question: string;
-    public yes: number = 1;
-    public no: number = 1;
-    
-    constructor(question: string, yes?: number, no?: number) {
-        this.question = question + "?";
-        this.yes = yes ?? this.yes;
-        this.no = no ?? this.yes ?? this.no;
+    question: QuestionEntryType;
+    yn: QuestionYesNoType;
+
+    constructor(question: QuestionEntryType, yn: Partial<QuestionYesNoType>) {
+        this.question = question;
+        this.yn = {
+            y: yn.y ?? 0,
+            n: yn.n ?? yn.y ?? 0,
+            inv: yn.inv ?? false,
+        };
+
+        if(this.yn.inv) {
+            this.yn = {
+                ...this.yn,
+                y: this.yn.n,
+                n: this.yn.y,
+            }
+        }
+    }
+
+    get yes() {
+        return this.yn.y;
+    }
+
+    get no() {
+        return this.yn.n;
+    }
+
+    scale(ynScale: Omit<QuestionYesNoType, "inv">) {
+        this.yn = {
+            ...this.yn,
+            y: this.yn.y / ynScale.y,
+            n: this.yn.n / ynScale.n,
+        };
     }
 }
 
 /**
  * Question Factory
- * @param q Question
+ * @param q Question and details
  * @param y Yes value
  * @param n No value
  * @returns Question Object
  */
-const Q = (q: string, y?: number, n?: number) => new Question(q, y, n);
+const Q = (q: QuestionEntryType, yn: Partial<QuestionYesNoType>) => new Question(q, yn);
 
+const inv = true;
 export const questions: Array<Question> = [
-    Q("Have I considered this for over two weeks", 3), // Reflection period importance
-    Q("Is this an impulsive purchase", 0, 5), // Impulse control
-    Q("Does it address a real problem I have", 5), // Necessity and problem-solving
-    Q("Do I already own something like it", 0, 4), // Avoiding redundancy
-    Q("Is it worth delaying financial goals", 5), // Financial goal significance
-    Q("Will I use it in five years", 2, 0), // Long-term utility
-    Q("Do I have space for it", 1), // Spatial consideration
-    Q("Will it lead to debt", 0, 5), // Debt avoidance
-    Q("Can I quickly pay for it", 4), // Financial flexibility
-    Q("Is there a better way to get it", 0, 3), // Alternative solutions
-    Q("Have I compared prices from different vendors", 3), // Price comparison
-    Q("Am I swayed by others", 0, 2), // External influence
-    Q("Is this purchase within my budget", 2, 5), // Budget alignment
-    Q("Is there a less expensive alternative", 0, 3), // Cost-effective options
-    Q("Will this purchase add value to my life", 4, 3), // Value addition
-    Q("Am I expecting a discount or sale soon", 3, 2), // Potential savings
-    Q("Could my money be better spent elsewhere", 4), // Opportunity cost
-    Q("Is this purchase influenced by a short-term emotion", 5), // Emotional buying
-    Q("Is this a limited-time offer influencing my decision", 3), // Scarcity influence
-    Q("Could I borrow or rent instead of buying", 2), // Ownership alternatives
+    Q(
+        ["Have I considered this for over two weeks"],
+        {
+            y: 3,
+        }, 
+    ),
+    Q(
+        ["Is this an impulsive purchase"],
+        {
+            y: 5,
+            n: 0,
+            inv,
+        }, 
+    ),
+    Q(
+        ["Does it address a real problem I have"],
+        {
+            y: 5,
+        }, 
+    ),
+    Q(
+        ["Do I already own something like it"],
+        {
+            y: 4,
+            n: 3,
+            inv,
+        }, 
+    ),
+    Q(
+        ["Is it worth delaying financial goals"],
+        {
+            y: 5,
+        }, 
+    ),
+    Q(
+        ["Will I use it in five years"],
+        {
+            y: 2,
+            n: 0,
+        }, 
+    ),
+    Q(
+        ["Do I have space for it"],
+        {
+            y: 1,
+        }, 
+    ),
+    Q(
+        ["Will it lead to debt"],
+        {
+            y: 5,
+            n: 0,
+            inv,
+        }, 
+    ),
+    Q(
+        ["Can I quickly pay for it"],
+        {
+            y: 4,
+        }, 
+    ),
+    Q(
+        ["Is there a better way to get it"],
+        {
+            y: 3,
+            n: 3,
+            inv,
+        }, 
+    ),
+    Q(
+        ["Have I compared prices from different vendors"],
+        {
+            y: 3,
+            n: 2,
+        }, 
+    ),
+    Q(
+        ["Am I swayed by others"],
+        {
+            y: 2,
+            n: 0,
+            inv,
+        }, 
+    ),
+    Q(
+        ["Is this purchase within my budget"],
+        {
+            y: 1,
+            n: 5,
+        }, 
+    ),
+    Q(
+        ["Is there a less expensive alternative"],
+        {
+            y: 3,
+            n: 0,
+            inv,
+        }, 
+    ),
+    Q(
+        ["Will this purchase add value to my life"],
+        {
+            y: 4,
+            n: 3,
+        }, 
+    ),
+    Q(
+        ["Am I expecting a discount or sale soon"], 
+        {
+            y: 3,
+            n: 0,
+            inv,
+        }, 
+    ),
+    Q(
+        ["Could my money be better spent elsewhere"], 
+        {
+            y: 4,
+            inv,
+        }, 
+    ),
+    Q(
+        ["Is this purchase influenced by a short-term emotion"], 
+        {
+            y: 5,
+            n: 0,
+            inv,
+        }, 
+    ),
+    Q(
+        ["Is this a limited-time offer influencing my decision"],
+        {
+            y: 3,
+            n: 0,
+            inv,
+        }, 
+    ),
+    Q(
+        ["Could I borrow or rent instead of buying"],
+        {
+            y: 2,
+            n: 0,
+            inv,
+        }, 
+    ),
 ];
 
 export const adjustmentYes = questions.reduce<number>((prev, curr) => {
-    return prev + curr.yes;
+    return prev + curr.yn.y;
 }, 0);
 
 export const adjustmentNo = questions.reduce<number>((prev, curr) => {
     return prev + curr.no;
 }, 0);
 
+const adjScale: Omit<QuestionYesNoType, "inv"> = {
+    y: adjustmentYes,
+    n: adjustmentNo,
+};
+
 export const deciderQuestions = questions.map((q) => {
-    q.yes /= adjustmentYes;
-    q.no /= adjustmentNo;
+    q.scale(adjScale);
     return Object.freeze(q);
 });
 
