8953e73b5ce560506158079ff44d493b27fd2f85
Lukasz Baldyga <rijndael@baldy.ga>
2024-04-16 14:41:16 +0100

    Updated grid layout and added answer toggle
    The grid layout in the decision module has been updated for better alignment of elements. The column proportions have been adjusted, and gaps and margins have been fine-tuned to improve the overall look.
    
    In addition, a new feature has been implemented in the decision page that allows users to cycle through different states (Unchecked, Checked, Not Applicable) by clicking on an answer. This is achieved by introducing an 'AnswerLoop' enum and a 'toggleAnswer' function which updates the state of answers accordingly.

--- src/app/portfolio/decision/page.tsx
+++ src/app/portfolio/decision/page.tsx
@@ -4,9 +4,39 @@ import Lazy from "@/components/lazy/_lazy";
 import { deciderP1, deciderQuestions } from "@/content/portfolio/decider/Decider";
 import { useState } from "react";
 import styles from "./decision.module.scss";
+import { RiCheckboxBlankFill, RiCheckboxFill, RiCheckboxIndeterminateFill, RiInformationFill, RiInformationOffFill } from "@remixicon/react";
+import { assert } from "console";
+
+enum AnswerLoop {
+    Unchecked,
+    Checked,
+    NotApplicable,
+}
 
 export default function Decider() {
-    const [answers, setAnswers] = useState<Array<boolean | undefined>>(new Array(deciderQuestions.length));
+    const [answers, setAnswers] = useState<Array<AnswerLoop | undefined>>(new Array(deciderQuestions.length));
+
+    const answerLoop = [
+        AnswerLoop.Unchecked,
+        AnswerLoop.Checked,
+        AnswerLoop.NotApplicable,
+    ];
+
+    const toggleAnswer = (index: number) => {
+        const newAnswers = [...answers];
+
+        // get current value
+        const answer = newAnswers[index] ?? AnswerLoop.Unchecked;
+
+        // get index in loop
+        const currentAnswerLoopIndex = answerLoop.indexOf(answer);
+
+        // set the next value
+        newAnswers[index] = answerLoop[(currentAnswerLoopIndex + 1) % answerLoop.length];
+
+        // set the next value
+        setAnswers(newAnswers);
+    }
 
     return <Lazy>
         <h1>Decision</h1>
@@ -22,13 +52,19 @@ export default function Decider() {
             </div>
             {
                 deciderQuestions.map((q, i) => {
-                    return <div 
-                    className={styles.question}
-                    key={i}>
-                        <div>{`${q.question}`}</div>
-                        <div className={styles.answer}>
-                            <div>Yes</div>
-                            <div>No</div>
+                    return <div
+                        className={styles.question}
+                        key={i}>
+                        <div>
+                            <p>{q.question}</p>
+                            <RiInformationFill />
+                        </div>
+                        <div className={styles.answer} onClick={() => toggleAnswer(i)}>
+                            <div>
+                                {
+                                    answers[i] == AnswerLoop.Checked ? <RiCheckboxFill /> : answers[i] == AnswerLoop.NotApplicable ? <RiCheckboxIndeterminateFill /> : <RiCheckboxBlankFill />
+                                }
+                            </div>
                         </div>
                     </div>;
                 })
