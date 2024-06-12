5daaf56246fb11682f5bf46a86d38ab1e63c6237
Rijndael <rijndael@baldy.ga>
2024-06-12 19:32:52 +0100

    Add ChooseRandomElement function, update Quotes component
    - Added ChooseRandomElement function to select random element from array
    - Updated Quotes component to display a random quote and author

--- src/components/app/footer/quotes.tsx
+++ src/components/app/footer/quotes.tsx
@@ -1,7 +1,13 @@
 "use server";
 
+import { ChooseRandomElement } from "@/components/util";
+import { quotes } from "@/content/footer/Quotes";
+
 export default async function Quotes() {
+    const quote = ChooseRandomElement(quotes);
+    const author = quote.author ?? "Unknown";
+
     return <h6>
-        Test
-    </h6>
+        {quote.eng} - {author}
+    </h6> 
 }
