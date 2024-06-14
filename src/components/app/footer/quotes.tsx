e678add012973751f4571f6aa247196ee51a49a0
Rijndael <rijndael@baldy.ga>
2024-06-14 13:55:13 +0100

    Add Link component for author's wiki page, update author properties.
    - Added a Link component to handle author's wiki page link in the Quotes component.
    - Updated the order of properties in the makeAuthor function to match usage in quotes.

--- src/components/app/footer/quotes.tsx
+++ src/components/app/footer/quotes.tsx
@@ -3,6 +3,8 @@
 import ToolTip from "@/components/toolTip/_toolTip";
 import { ChooseRandomElement } from "@/components/util";
 import { quotes } from "@/content/footer/Quotes";
+import Link from "next/link";
+import { ReactNode } from "react";
 
 function ChooseQuote({ text, og }: { text: string, og?: string }) {
     return og ?
@@ -10,13 +12,19 @@ function ChooseQuote({ text, og }: { text: string, og?: string }) {
         <>{text}</>
 }
 
+function ChooseAuthor({ children, link }: { children: ReactNode, link?: string }) {
+    return link ?
+        <Link href={link}>{children}</Link> :
+        children
+}
+
 export default async function Quotes() {
     const quote = ChooseRandomElement(quotes);
     const author = quote.author.engName ?? quote.author.name;
     const text = quote.eng ?? quote.og;
 
-    return <h6 style={{flexDirection: "column"}}>
+    return <h6 style={{ flexDirection: "column" }}>
         <span>"<ChooseQuote text={text} og={quote.og} />"</span>
-        <span>{author}</span>
+        <span><ChooseAuthor link={quote.author.wiki}>{author}</ChooseAuthor></span>
     </h6>
 }
--- src/content/footer/Quotes.ts
+++ src/content/footer/Quotes.ts
@@ -10,11 +10,11 @@ export interface Quote {
     author: Author,
 }
 
-function makeAuthor(name: string, engName?: string, wiki?: string): Author {
+function makeAuthor(name: string, wiki?: string, engName?: string): Author {
     return {
         name,
-        engName,
         wiki,
+        engName,
     };
 }
 
@@ -32,59 +32,89 @@ export const quotes: Array<Quote> = [
     makeQuote(
         "W życiu piękne są tylko chwile.",
         "In life, only moments are beautiful",
-        makeAuthor("Ryszard Riedel")
+        makeAuthor(
+            "Ryszard Riedel",
+            "https://en.wikipedia.org/wiki/Ryszard_Riedel",
+        ),
     ),
 
     makeQuote(
         "Kiedy śmieje się dziecko, śmieje się cały świat.",
         "When a child laughs, the whole world laughs.",
-        makeAuthor("Janusz Korczak"),
+        makeAuthor(
+            "Janusz Korczak",
+            "https://en.wikipedia.org/wiki/Janusz_Korczak",
+        ),
     ),
 
     makeQuote(
         "Nie żałuj że się starzejesz. To przywilej wielu nie dany.",
-        "Don't regret getting older. It's a privilege denied to many."),
+        "Don't regret getting older. It's a privilege denied to many."
+    ),
 
     makeQuote(
         "Człowiek jest wielki nie przez to, co posiada, lecz przez to, kim jest; nie przez to, co ma, lecz przez to, czym dzieli się z innymi.",
         "A person is not great by what they own, but by what they are; not by what they have, but by what they share with others.",
-        makeAuthor("Pope John Paul II - Karol Wojtyła"),
+        makeAuthor(
+            "Jan Paweł II - Karol Wojtyła",
+            "https://en.wikipedia.org/wiki/Pope_John_Paul_II",
+            "Pope John Paul II - Karol Wojtyła",
+        ),
     ),
 
     makeQuote(
         "Lepiej zaliczać się do niektórych, niż do wszystkich.",
         "It's better to belong to some than to everyone.",
-        makeAuthor("Andrzej Sapkowski"),
+        makeAuthor(
+            "Andrzej Sapkowski",
+            "https://en.wikipedia.org/wiki/Andrzej_Sapkowski",
+        ),
     ),
 
     makeQuote(
         "Nie ma nic lepszego niż możliwość przelania na papier myśli.",
         "There is nothing better than the ability to put thoughts on paper.",
-        makeAuthor("Maria Skłodowska - Curie"),
+        makeAuthor(
+            "Maria Salomea Skłodowska-Curie",
+            "https://en.wikipedia.org/wiki/Marie_Curie",
+            "Marie Curie",
+        ),
     ),
 
     makeQuote(
         "Świat jest piękny, ale jeżeli ktoś wątpi w to, niech odnajdzie siebie.",
         "The world is beautiful, but if someone doubts it, let them find themselves.",
-        makeAuthor("Adam Mickiewicz"),
+        makeAuthor(
+            "Adam Bernard Mickiewicz",
+            "https://en.wikipedia.org/wiki/Adam_Mickiewicz",
+        ),
     ),
 
     makeQuote(
         "Prawdziwa odwaga polega na robieniu tego, czego się boisz.",
         "True courage consists in doing what you fear.",
-        makeAuthor("Józef Piłsudski"),
+        makeAuthor(
+            "Józef Piłsudski",
+            "https://en.wikipedia.org/wiki/J%C3%B3zef_Pi%C5%82sudski",
+        ),
     ),
 
     makeQuote(
         "Strach nie istnieje nigdzie indziej, tylko w umyśle.",
         "Fear does not exist anywhere else but in the mind.",
-        makeAuthor("Stefan Żeromski"),
+        makeAuthor(
+            "Stefan Żeromski",
+            "https://en.wikipedia.org/wiki/Stefan_%C5%BBeromski",
+        ),
     ),
 
     makeQuote(
         "Niech twoje życie będzie świadectwem, że niemożliwe nie istnieje.",
         "Let your life be a testimony that impossible does not exist.",
-        makeAuthor("Wisława Szymborska"),
+        makeAuthor(
+            "Maria Wisława Anna Szymborska",
+            "https://en.wikipedia.org/wiki/Wis%C5%82awa_Szymborska",
+        ),
     ),
 
 ];
