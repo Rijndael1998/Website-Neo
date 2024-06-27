7fd9080f2cc62ad6bc87f5f11c4fed1190b9fd41
Rijndael <rijndael@baldy.ga>
2024-06-27 20:57:37 +0100

    made text and subtitle optional

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -9,8 +9,6 @@ export default function Blog() {
         groups={
             [
                 {
-                    title: "Tech",
-                    subtitle: "Tech related blogs",
                     groups: [
                         {
                             title: MirrorTitle,
--- src/components/group/_groupPageLayout.tsx
+++ src/components/group/_groupPageLayout.tsx
@@ -2,16 +2,16 @@ import styles from "./group.module.scss";
 import GroupItem from "./_groupItem";
 import { GroupViewArray } from "./_groupTypes";
 
-export type GroupPageLayoutProps = { 
-    title: string, 
-    subtitle: string,
+export type GroupPageLayoutProps = {
+    title?: string,
+    subtitle?: string,
     groups: GroupViewArray,
 };
 
 export default function GroupPageLayout({ groups, title, subtitle }: GroupPageLayoutProps) {
     return <>
-        <h2>{title}</h2>
-        <p>{subtitle}</p>
+        {title ? <h2>{title}</h2> : <></>}
+        {subtitle ? <p>{subtitle}</p> : <></>}
         <div className={styles.content}>
             {
                 groups.map(
