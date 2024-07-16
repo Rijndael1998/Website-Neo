7c7d0f85b919336c02f009b8a6eb7f36a157d179
Rijndael <rijndael@baldy.ga>
2024-07-16 04:15:40 +0100

    added tooltips to copies

--- src/components/toolTip/_toolTip.tsx
+++ src/components/toolTip/_toolTip.tsx
@@ -5,15 +5,29 @@ import Tooltip from '@mui/material/Tooltip';
 export type ToolTipProps = {
     children: ReactElement,
     tip: string,
+    className?: string,
+    offset?: [x: number, y: number];
 }
 
-export default function ToolTip({ children, tip }: ToolTipProps) {
+export default function ToolTip({ children, tip, className, offset }: ToolTipProps) {
     return (
         <Tooltip
-            className={styles.tooltip}
+            className={className ?? styles.tooltip}
             title={tip}
             placement="top"
             arrow
+            slotProps={offset ? {
+                popper: {
+                  modifiers: [
+                    {
+                      name: 'offset',
+                      options: {
+                        offset: offset,
+                      },
+                    },
+                  ],
+                },
+              } : undefined}
         >
             {children}
         </Tooltip>
