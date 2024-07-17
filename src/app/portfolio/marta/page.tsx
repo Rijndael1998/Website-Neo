035bf63b061dd475e64455adbd002cbac04daffe
Rijndael <rijndael@baldy.ga>
2024-07-17 02:52:05 +0100

    text revision

--- src/app/portfolio/marta/page.tsx
+++ src/app/portfolio/marta/page.tsx
@@ -1,55 +1,61 @@
-"use server";
-
 import ImageGallery from "@/components/imageGallery/_imageGallery";
 import Lazy from "@/components/lazy/_lazy";
 import LazyImage from "@/components/lazy/_lazyImage";
 import { content, citations } from "@/content/portfolio/marta/Marta";
 
+const horizontal_aspect_ratio = 2016 / 1512;
+const vertical_aspect_ratio = 1512 / 2016;
+
+function Gallery() {
+    return <ImageGallery
+        aspectRatio={horizontal_aspect_ratio}
+        images={[
+            {
+                src: "/fun/marta/1.jpg",
+            },
+            {
+                src: "/fun/marta/2.jpg",
+            },
+            {
+                src: "/fun/marta/3.jpg",
+            },
+            {
+                src: "/fun/marta/4.jpg",
+            },
+            {
+                src: "/fun/marta/v2.jpg",
+                aspectRatio: vertical_aspect_ratio,
+            },
+            {
+                src: "/fun/marta/v3.jpg",
+                aspectRatio: vertical_aspect_ratio,
+            },
+            {
+                src: "/fun/marta/v4.jpg",
+                aspectRatio: vertical_aspect_ratio,
+            },
+            {
+                src: "/fun/marta/v5.jpg",
+                aspectRatio: vertical_aspect_ratio,
+            },
+        ]}
+    />
+}
+
 export default async function Marta() {
     let i = 0; // this shouldn't be working... 🤯
 
     return <Lazy>
-        <h1>Marta</h1>
+        <h1>Marta Fundraiser</h1>
+
+        <Gallery />
 
         <p>{content[i++]}</p>
         <p>{content[i++]}</p>
         <p>{content[i++]}</p>
-        <ImageGallery
-            aspectRatio={2016 / 1512}
-            images={[
-                {
-                    src: "/fun/marta/1.jpg",
-                },
-                {
-                    src: "/fun/marta/2.jpg",
-                },
-                {
-                    src: "/fun/marta/3.jpg",
-                },
-                {
-                    src: "/fun/marta/4.jpg",
-                },
-                {
-                    src: "/fun/marta/v2.jpg",
-                    aspectRatio: 1512 / 2016,
-                },
-                {
-                    src: "/fun/marta/v3.jpg",
-                    aspectRatio: 1512 / 2016,
-                },
-                {
-                    src: "/fun/marta/v4.jpg",
-                    aspectRatio: 1512 / 2016,
-                },
-                {
-                    src: "/fun/marta/v5.jpg",
-                    aspectRatio: 1512 / 2016,
-                },
-            ]}
-        />
         <p>{content[i++]}</p>
 
-        <LazyImage src="/fun/marta/v1.jpg" alt="" aspectRatio={1512 / 2016}/>
+        <LazyImage src="/fun/marta/v1.jpg" alt="" aspectRatio={vertical_aspect_ratio} />
         <p>{content[i++]}</p>
 
         <h3>Referances</h3>
--- src/content/portfolio/marta/Marta.tsx
+++ src/content/portfolio/marta/Marta.tsx
@@ -2,19 +2,27 @@ import { LazyLink } from "@/components/lazy/_lazyLink";
 import Link from "next/link";
 
 export const content = [
-    "Taking care of three small children is not easy. Being a single mother is harder. Battling cancer is incredibly challenging beyond words. Yet, it showcases your immense strength, resilience, and determination. This was the reality for our friend Marta. She was suffering from breast cancer, a cancer likely to affect 1 in 8 women, while also being a single parent to three children.",
+    "Taking care of three small children is no easy task. Being a single mother adds to the challenge, and facing the battle with cancer can feel overwhelming. Our friend Marta embodied remarkable strength, resilience, and determination during such a difficult time. She was fighting breast cancer, an illness that affects 1 in 8 women, while also raising her three children on her own.",
 
-    "In December of 2022, Marta had very little, mostly just the essentials, and she was struggling to get by. She didn't have much furniture: an old table set, some drawers, a small sofa, and some chairs. One of the problems we recognized was her difficulty in drying clothes after they were washed. She was air-drying clothes on a simple clothes dryer, which often took over 24 hours to dry because of the poor weather. We also noted that she did not have a place to store clothes. She also expressed difficulty studying for her university course online.",
+    "By December 2022, Marta was struggling with only the bare essentials to get by. She had minimal furniture: an old table set, some drawers, a small sofa, and a few chairs. One of the issues she faced was finding an efficient way to dry clothes, as she relied on a simple clothes dryer that often took over 24 hours to dry due to the poor weather. Additionally, she didn't have sufficient storage for clothes and expressed challenges in studying for her university course online.",
 
-    <>{"My mom, Matylda, wanted to help her but neither she or I had the means to do so. We have helped her in the past with smaller things, but this was too big for the two of us to do alone. My mom turned to her "}<Link href="https://matilda-gifts.shop/">{"business"}</Link>{" and recognised that she could raise some money by selling candles that she made, and then use the revenue to get the things that Marta needed. The only problem that we had was the location to sell the candles."}</>,
+    <>
+        {"My mom, Matylda, and I wanted to assist her in a meaningful way. Although we had helped her with smaller tasks in the past, this time the need was greater than what we could manage on our own. My mom, who owns a small business, recognized an opportunity to raise some funds by selling handmade candles through her "}
+        <Link href="https://matilda-gifts.shop/">{"shop"}</Link>
+        {". However, finding a suitable place to sell the candles was a challenge."}
+    </>,
 
-    "Me and my mom were close friends with a priest in Bolton overseeing a Polish Catholic mission. He very politely allowed us to sell the candles at the churches that he performed mass in. ",
+    "We turned to a close family friend, a priest in Bolton overseeing a Polish Catholic mission, who graciously allowed us to sell the candles at the churches where he held masses.",
 
-    <>{"We sold sold almost all of the candles and raised a total of £471. I also reached out to my employer, "} <Link href="/portfolio/ipf">IPF</Link>{", and they boosted the amount by £250. We used this money to buy her the furniture, a clothes drier and a new laptop for her classes along with gifts for her children for Christmas."}</>,
+    <>
+        {"Nearly all of the candles were sold, and we managed to raise a total of £471. In addition, my employer, "}
+        <Link href="/portfolio/ipf">{"IPF"}</Link>
+        {", generously contributed an additional £250. With the combined funds, we were able to purchase new furniture, a clothes drier, and a new laptop for Marta to use in her studies, along with Christmas gifts for her children."}
+    </>,
 ];
 
 export const citations = [
-    <LazyLink link="https://www.breastcancer.org/facts-statistics" />,
+    <LazyLink key={"link1"} link="https://www.breastcancer.org/facts-statistics" />,
     <>
         American Cancer Society: “Breast Cancer Facts & Figures 2022-2024,” “Cancer Facts & Figures 2023.”
         Available at: <LazyLink link="https://www.cancer.org/content/dam/cancer-org/research/cancer-facts-and-statistics/breast-cancer-facts-and-figures/2022-2024-breast-cancer-fact-figures-acs.pdf" /> and <LazyLink link="https://www.cancer.org/content/dam/cancer-org/research/cancer-facts-and-statistics/annual-cancer-facts-and-figures/2023/2023-cancer-facts-and-figures.pdf" />.
