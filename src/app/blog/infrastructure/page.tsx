8604fd48595843a963433dae86cabec1d4fdc720
Rijndael <rijndael@baldy.ga>
2024-07-29 02:16:36 +0100

    md is now working

--- src/app/blog/infrastructure/page.tsx
+++ src/app/blog/infrastructure/page.tsx
@@ -2,13 +2,17 @@
 
 import Lazy from "@/components/lazy/_lazy";
 import { getFile } from "@/components/serverUtils";
+import Markdown from "react-markdown";
+import remarkGfm from 'remark-gfm';
 
 export default async function Infastructure() {
+    const pageContent = await getFile("/src/content/blog/infra.md");
+
     return <Lazy>
         <p>
-            {
-                await getFile("/src/content/blog/infra.md")
-            }
+            <Markdown remarkPlugins={[remarkGfm]}>
+                {pageContent}
+            </Markdown>
         </p>
     </Lazy>
 }
