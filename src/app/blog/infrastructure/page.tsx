d86e975488302cbd14eed579fe27548e5a4cc9c7
Rijndael <rijndael@baldy.ga>
2024-07-29 03:57:00 +0100

    mermaidjs doesn't work

--- src/app/blog/infrastructure/page.tsx
+++ src/app/blog/infrastructure/page.tsx
@@ -5,13 +5,14 @@ import { getFile } from "@/components/serverUtils";
 import Markdown from "react-markdown";
 import remarkGfm from 'remark-gfm';
 import remarkRehype from "remark-rehype";
-import remarkMermaid from "rehype-mermaidjs";
+import rehypeMermaid from "rehype-mermaidjs";
+import Script from "next/script";
 
 export default async function Infastructure() {
     const pageContent = await getFile("/src/content/blog/infra.md");
 
     return <Lazy>
-        <Markdown remarkPlugins={[remarkGfm, remarkRehype, remarkMermaid]}>
+        <Markdown remarkPlugins={[remarkGfm, remarkRehype]}>
             {pageContent}
         </Markdown>
     </Lazy>
