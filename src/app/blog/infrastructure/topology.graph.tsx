430036ff1ad9c8b01d37d887cd52f34c60289a6c
Rijndael <rijndael@baldy.ga>
2024-08-05 17:32:05 +0100

    flow works

--- src/app/blog/infrastructure/topology.graph.tsx
+++ src/app/blog/infrastructure/topology.graph.tsx
@@ -1,18 +1,29 @@
 import React from 'react';
-import { ReactFlow } from '@xyflow/react';
+import {
+  Background,
+  Controls,
+  MiniMap,
+  ReactFlow,
+  ReactFlowProps,
+} from "@xyflow/react";
 
 import '@xyflow/react/dist/style.css';
 
-const initialNodes = [
+const initialNodes: ReactFlowProps["nodes"] = [
   { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
   { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
 ];
-const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
+const initialEdges = [{ id: 'e1->2', source: '1', target: '2', animated: true }];
 
 export default function layoutFlow() {
   return (
-    <div style={{width: "100%", height: "50ex", resize: "vertical"}}>
-      <ReactFlow colorMode='dark' nodes={initialNodes} edges={initialEdges} />
+    <div style={{ width: "100%", height: "50ex", resize: "vertical" }}>
+      <ReactFlow colorMode='dark' nodes={initialNodes} edges={initialEdges} fitView
+      >
+        <Background />
+        <MiniMap />
+        <Controls />
+      </ReactFlow>
     </div>
   )
 }
---
+++ src/components/flow/Flow.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import {
+    Background,
+    Controls,
+    MiniMap,
+    ReactFlow,
+    ReactFlowProps,
+} from "@xyflow/react";
+
+export type FlowProps = {
+    flowProps: Omit<ReactFlowProps, "fitView" | "colorMode">,
+    style?: React.CSSProperties,
+    bg?: boolean,
+    mm?: boolean,
+    co?: boolean,
+};
+
+const show = (bool: boolean, part: React.ReactNode) => {
+    if (!bool) return <></>
+    return part;
+}
+
+export default function Flow({ flowProps, style, bg, mm, co }: FlowProps) {
+    const newStyle = style ?? { width: "100%", height: "50ex", resize: "vertical" };
+    const showBG = bg ?? true;
+    const showMM = mm ?? false;
+    const showCO = co ?? true;
+
+    return <div style={newStyle}>
+        <ReactFlow {...flowProps} colorMode='dark' fitView
+        >
+            {show(showBG, <Background />)}
+            {show(showMM, <MiniMap />)}
+            {show(showCO, <Controls />)}
+        </ReactFlow>
+    </div>
+}
