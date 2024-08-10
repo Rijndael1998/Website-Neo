9a02b6379650f4ae839f03518b2979a62f4f0548
Rijndael <rijndael@baldy.ga>
2024-08-11 00:08:55 +0100

    added icons

--- src/components/group/_groupItem.tsx
+++ src/components/group/_groupItem.tsx
@@ -4,15 +4,27 @@ import GroupImage from "./groupImage/_groupImage";
 import { preURL } from "@/content/portfolio/Portfolio";
 import classNames from "classnames";
 import { GroupPreviewContent } from "./_groupTypes";
-
+import LaunchIcon from '@mui/icons-material/Launch';
+import LinkIcon from '@mui/icons-material/Link';
+import { SvgIcon } from "@mui/material";
 
 export type GroupProps = {
     portfolio: GroupPreviewContent,
 }
 
+export type LinkText = "" | "See Demo" | "External Link" | "Details";
+
+export const IconMatrix: Map<LinkText, typeof SvgIcon> = new Map([
+    ["External Link", LaunchIcon],
+    ["See Demo", LaunchIcon],
+    ["Details", LinkIcon],
+    ["", LinkIcon],
+]);
+
 export default function GroupItem({ portfolio }: GroupProps) {
     const empty = portfolio.url == null || portfolio.url == "";
-    const text = empty ? "" : portfolio.isDemo ? "See Demo" : (portfolio.url!.includes("https") ? "External Link" : "Details");
+    const text: LinkText = empty ? "" : portfolio.isDemo ? "See Demo" : (portfolio.url!.includes("https") ? "External Link" : "Details");
+    const Icon = IconMatrix.get(text) ?? IconMatrix.get("")!;
 
     return <div className={classNames(styles.portfolio, empty && styles.empty)}>
         {
@@ -22,12 +34,16 @@ export default function GroupItem({ portfolio }: GroupProps) {
             <h2>
                 {portfolio.title}
             </h2>
+        </div>
+
+        <div className={styles.portfolioMainContentHover}>
             {/* <p>
                 {portfolio.desc}
             </p> */}
+            {/* {
+                !empty && <GroupButton url={portfolio.url ?? ""} text={text} />
+            } */}
+            <Icon />
         </div>
-        {/* {
-            !empty && <GroupButton url={portfolio.url ?? ""} text={text} />
-        } */}
     </div>
 }
