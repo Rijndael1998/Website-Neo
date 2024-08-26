ce33fee370c074754ec40d0ce62e8f210dc14c39
Rijndael <rijndael@baldy.ga>
2024-08-26 19:59:10 +0100

    fixed metadata issue with h1 tags

--- src/components/app/header/title/_title.tsx
+++ src/components/app/header/title/_title.tsx
@@ -4,14 +4,14 @@ import styles from "./title.module.scss";
 export default function Title({ text }: { text: string }) {
     return <>
         <div className={styles.wrapper}>
-            <h1>{
+            <h2>{
                 text.split(" ").map((word, index) => {
                     return <span className={styles.span} key={index}>
                         <Typing text={word} interval={100} iterCount={14} />
                     </span>
                 })
             }
-            </h1>
+            </h2>
         </div>
     </>
 } 
