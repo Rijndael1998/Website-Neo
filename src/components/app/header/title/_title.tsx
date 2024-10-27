a21aa7cb6e6682c1499ca336e3320a95f5c004be
Rijndael <rijndael@baldy.ga>
2024-10-27 18:51:02 +0000

    Title now goes back home, exactly like on every other website

--- src/components/app/header/title/_title.tsx
+++ src/components/app/header/title/_title.tsx
@@ -1,16 +1,18 @@
+import Link from "next/link";
 import styles from "./title.module.scss";
 
 export default function Title({ text }: { text: string }) {
-    return <>
+    return <Link href={"/"} style={{ color: "white", "textDecoration": "none" }}>
         <div className={styles.wrapper}>
-            <h2>{
-                text.split(" ").map((word, index) => {
-                    return <span className={styles.span} key={index}>
-                        {word}
-                    </span>
-                })
-            }
+            <h2>
+                {
+                    text.split(" ").map((word, index) => {
+                        return <span className={styles.span} key={index}>
+                            {word}
+                        </span>
+                    })
+                }
             </h2>
         </div>
-    </>
+    </Link>
 } 
