bb8727e45a6815a7b006e9d43085ed70f6852cd4
Rijndael <rijndael@baldy.ga>
2024-10-27 18:10:19 +0000

    Added proper icons

--- src/components/app/nav/navLinkCollection/navLink/links.tsx
+++ src/components/app/nav/navLinkCollection/navLink/links.tsx
@@ -1,11 +1,11 @@
-import FileCopyIcon from '@mui/icons-material/FileCopyOutlined';
-import SaveIcon from '@mui/icons-material/Save';
-import PrintIcon from '@mui/icons-material/Print';
-import ShareIcon from '@mui/icons-material/Share';
+import HomeIcon from '@mui/icons-material/Home';
+import WorkIcon from '@mui/icons-material/Work';
+import BookIcon from '@mui/icons-material/Book';
+import TimelineIcon from '@mui/icons-material/Timeline';
 
 export const Links: Array<[title: string, url: string, icon: React.ReactNode]> = [
-    ["Home", "/", <FileCopyIcon />,],
-    ["Portfolio", "/portfolio", <SaveIcon />,],
-    ["Blog", "/blog", <PrintIcon />,],
-    ["Timeline", "/timeline", <ShareIcon />,],
+    ["Home", "/", <HomeIcon />,],
+    ["Portfolio", "/portfolio", <WorkIcon />,],
+    ["Blog", "/blog", <BookIcon />,],
+    ["Timeline", "/timeline", <TimelineIcon />,],
 ];
