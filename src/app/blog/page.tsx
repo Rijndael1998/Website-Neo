09467cdba0e33dbf902393771b60ffe594965394
Rijndael <rijndael@baldy.ga>
2024-10-28 00:38:25 +0000

    added the descriptions to the blog posts

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -38,6 +38,34 @@ const crab: GroupPreviewContent = {
     image: imageFolder + "crab_rave.png",
 };
 
+const OpenWRT: GroupPreviewContent = {
+    title: "OpenWRT 5.15 on Octaplus networks",
+    desc: `
+    This article serves as a detailed guide for setting up OpenWRT 5.15 on an Octaplus network using a custom router. It highlights the basic configuration steps needed for users to connect their routers to CityFibre's fiberoptic internet service through Octaplus. Key instructions include tagging the VLAN with 911, configuring device settings using the OpenWRT interface, setting up PPPoE with specific username and password instructions, and ensuring proper network interface configuration. The guide also addresses potential challenges, such as MAC address binding and troubleshooting advice to ensure a successful connection.
+    `,
+    isDemo: false,
+    url: "blog/city_fibre",
+    image: `${preURL}city_fibre/logos.webp`,
+};
+
+const Ender: GroupPreviewContent = {
+    title: "Recover your Creality Ender-3 V3 SE",
+    desc: `
+    In this article, I recount my harrowing experience of trying to revive a borrowed Creality Ender-3 V3 SE after a failed Klipper installation. Spoiler alert: I aged a few years in the process! I'll walk you through the lesser-known, crucial steps to properly format your SD card to meet the 3D printer's finicky requirements and explain why updating the screen firmware is just as important as updating the main firmware. Learn from my mishaps and avoid your own 3D printing nightmares with these handy recovery tips.
+    `,
+    isDemo: false,
+    url: "blog/3d_printer",
+    image: `${preURL}3d printer/3d printer.webp`,
+};
+
+const Mirror: GroupPreviewContent = {
+    title: MirrorTitle,
+    desc: MirrorSynopsis,
+    isDemo: false,
+    url: "blog/mirrors_edge",
+    image: `${preURL}mirrors_edge/1x1 partial logo.png`,
+};
+
 export default function Blog() {
     return <GroupPage
         title={"Blog"}
@@ -47,32 +75,14 @@ export default function Blog() {
                     title: "Tech",
                     subtitle: "Tech related blogs",
                     groups: [
-                        {
-                            title: "OpenWRT 5.15 on Octaplus networks",
-                            desc: "",
-                            isDemo: false,
-                            url: "blog/city_fibre",
-                            image: `${preURL}city_fibre/logos.webp`,
-                        },
-                        {
-                            title: "Recover your Creality Ender-3 V3 SE",
-                            desc: "",
-                            isDemo: false,
-                            url: "blog/3d_printer",
-                            image: `${preURL}3d printer/3d printer.webp`,
-                        },
-                        {
-                            title: MirrorTitle,
-                            desc: MirrorSynopsis,
-                            isDemo: false,
-                            url: "blog/mirrors_edge",
-                            image: `${preURL}mirrors_edge/1x1 partial logo.png`,
-                        },
+                        Ender,
+                        OpenWRT,
+                        Mirror,
                     ],
                 },
                 {
                     title: "Extra things",
-                    subtitle: "",
+                    subtitle: "Miscellaneous things I could not find a place for",
                     groups: [
                         Marta,
                         crab,
