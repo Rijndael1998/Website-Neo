acf8198a8fbe58bd71ef46c19e0db4cf31d06243
Rijndael <rijndael@baldy.ga>
2024-11-23 10:09:25 +0000

    semi converted mirror's edge

---
+++ src/app/blog/mirrors_edge/images/index.tsx
@@ -0,0 +1,118 @@
+import logo from "./../opengraph-image.jpg";
+import partial_1x1 from "./1x1 partial logo.png";
+import partial_1x1_200 from "./1x1 partial logo 200.png";
+
+import blender_comp from "./blender/compositor.png";
+import blender_final from "./blender/final.png";
+import blender_hdri from "./blender/hdri.png";
+import blender_raw from "./blender/raw.png";
+import blender_world from "./blender/world.png";
+
+import render1 from "./renders/1.png"
+import render2 from "./renders/2.png"
+import render3 from "./renders/3.png"
+import render4 from "./renders/4.png"
+import render5 from "./renders/5.png"
+import render6 from "./renders/6.png"
+import render7 from "./renders/7.png"
+import render8 from "./renders/8.png"
+import render9 from "./renders/9.png"
+
+import screenshot1 from "./screenshots/screenshot1.jpg";
+import screenshot2 from "./screenshots/screenshot2.jpg";
+import screenshot3 from "./screenshots/screenshot3.jpg";
+import screenshot4 from "./screenshots/screenshot4.jpg";
+import screenshot5 from "./screenshots/screenshot5.jpg";
+import screenshot6 from "./screenshots/screenshot6.jpg";
+import screenshot7 from "./screenshots/screenshot7.jpg";
+import screenshot8 from "./screenshots/screenshot8.jpg";
+import screenshot9 from "./screenshots/screenshot9.jpg";
+import screenshot10 from "./screenshots/screenshot10.jpg";
+import screenshot11 from "./screenshots/screenshot11.jpg";
+import screenshot12 from "./screenshots/screenshot12.jpg";
+import screenshot13 from "./screenshots/screenshot13.jpg";
+import screenshot14 from "./screenshots/screenshot14.jpg";
+import screenshot15 from "./screenshots/screenshot15.jpg";
+import screenshot16 from "./screenshots/screenshot16.jpg";
+import screenshot17 from "./screenshots/screenshot17.jpg";
+import screenshot18 from "./screenshots/screenshot18.jpg";
+import screenshot19 from "./screenshots/screenshot19.jpg";
+import screenshot20 from "./screenshots/screenshot20.jpg";
+import screenshot21 from "./screenshots/screenshot21.jpg";
+import screenshot22 from "./screenshots/screenshot22.jpg";
+import screenshot23 from "./screenshots/screenshot23.jpg";
+import screenshot24 from "./screenshots/screenshot24.jpg";
+import screenshot25 from "./screenshots/screenshot25.jpg";
+import screenshot26 from "./screenshots/screenshot26.jpg";
+import screenshot27 from "./screenshots/screenshot27.jpg";
+import screenshot28 from "./screenshots/screenshot28.jpg";
+import screenshot29 from "./screenshots/screenshot29.jpg";
+import screenshot30 from "./screenshots/screenshot30.jpg";
+import screenshot31 from "./screenshots/screenshot31.jpg";
+import screenshot32 from "./screenshots/screenshot32.jpg";
+import screenshot33 from "./screenshots/screenshot33.jpg";
+
+export {
+    logo,
+    partial_1x1,
+    partial_1x1_200,
+
+    blender_comp,
+    blender_final,
+    blender_hdri,
+    blender_raw,
+    blender_world,
+
+    render1,
+    render2,
+    render3,
+    render4,
+    render5,
+    render6,
+    render7,
+    render8,
+    render9,
+
+    screenshot1,
+    screenshot2,
+    screenshot3,
+    screenshot4,
+    screenshot5,
+    screenshot6,
+    screenshot7,
+    screenshot8,
+    screenshot9,
+    screenshot10,
+    screenshot11,
+    screenshot12,
+    screenshot13,
+    screenshot14,
+    screenshot15,
+    screenshot16,
+    screenshot17,
+    screenshot18,
+    screenshot19,
+    screenshot20,
+    screenshot21,
+    screenshot22,
+    screenshot23,
+    screenshot24,
+    screenshot25,
+    screenshot26,
+    screenshot27,
+    screenshot28,
+    screenshot29,
+    screenshot30,
+    screenshot31,
+    screenshot32,
+    screenshot33,
+};
+
+// there needs to be a better way than writing a one off python script to generate this code...
+/*
+for x in range(1, 34):
+    print(f"import screenshot{x} from \"./screenshots/screenshot{x}.jpg\";")
+
+for x in range(1, 34):
+    print(f"screenshot{x},")
+*/
