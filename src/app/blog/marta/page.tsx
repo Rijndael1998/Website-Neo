d90a85d14dd2ba8c2e461aa63f831b609b10b3b1
Rijndael <rijndael@baldy.ga>
2024-11-24 21:01:04 +0000

    exported gallery

--- src/app/blog/marta/page.tsx
+++ src/app/blog/marta/page.tsx
@@ -1,52 +1,13 @@
-import ImageGallery from "@/components/imageGallery/_imageGallery";
 import Lazy from "@/components/lazy/_lazy";
 import LazyLocalImage from "@/components/lazy/_lazyLocalImage";
 import { content, citations } from "@/content/portfolio/marta/Marta";
 import * as images from "./images";
+import { Gallery } from "./gallery";
 
 export const metadata = {
     title: "Marta Fundraiser's",
     description: content[0] as string,
 };
-
-const horizontal_aspect_ratio = 2016 / 1512;
-const vertical_aspect_ratio = 1512 / 2016;
-
-function Gallery() {
-    return <ImageGallery
-        aspectRatio={horizontal_aspect_ratio}
-        images={[
-            {
-                src: images.i1,
-            },
-            {
-                src: images.i2,
-            },
-            {
-                src: images.i3,
-            },
-            {
-                src: images.i4,
-            },
-            {
-                src: images.v2,
-                aspectRatio: vertical_aspect_ratio,
-            },
-            {
-                src: images.v3,
-                aspectRatio: vertical_aspect_ratio,
-            },
-            {
-                src: images.v4,
-                aspectRatio: vertical_aspect_ratio,
-            },
-            {
-                src: images.v5,
-                aspectRatio: vertical_aspect_ratio,
-            },
-        ]}
-    />
-}
 
 export default function Marta() {
     return <Lazy>
