42c7e06c7445ab69ec27d56c2b36f50d475a3ba3
Rijndael <rijndael@baldy.ga>
2024-11-25 17:26:05 +0000

    type def

---
+++ src/mdx.d.ts
@@ -0,0 +1,9 @@
+declare module "*.mdx" {
+  let MDXComponent: (props: any) => JSX.Element;
+  export default MDXComponent;
+
+  export const metadata: {
+    title: string,
+    description: string,
+  };
+};
