19a811cb52d04a0da8a5c5f632bfa53b977aad23
Rijndael <rijndael@baldy.ga>
2024-11-30 23:00:01 +0000

    modified page

--- src/app/blog/advent_of_code/[day]/page.tsx
+++ src/app/blog/advent_of_code/[day]/page.tsx
@@ -1,9 +1,11 @@
-export default async function AdventOfCodeDays({
-    params,
-  }: {
-    params: Promise<{ day: string }>
-  }) {
+export type AdventOfCodeDaysProps = {
+    params: Promise<{ day: string }>,
+};
+
+export default async function AdventOfCodeDays({ params }: AdventOfCodeDaysProps) {
     const paramsDay = (await params).day;
+
+
 
     return <p>Post: {paramsDay}</p>
 }
