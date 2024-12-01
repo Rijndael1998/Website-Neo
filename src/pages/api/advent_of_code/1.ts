49e4b41c356ea7744d3dd29b02e632c89868e772
Rijndael <rijndael@baldy.ga>
2024-12-01 12:29:24 +0000

    part 1 ready

--- src/pages/api/advent_of_code/1.ts
+++ src/pages/api/advent_of_code/1.ts
@@ -1,8 +1,35 @@
 import { AdventOfCodeSolutionFunction } from "./solutions";
 
 export const solution_1: AdventOfCodeSolutionFunction = (input) => {
-    
+    const left: Array<number> = [];
+    const right: Array<number> = [];
 
-    return "";
+    const lines = input.split("\n");
+
+    for (let index = 0; index < lines.length; index++) {
+        const element = lines[index].trim();
+        if(!element)
+            continue;
+
+        const leftRight = element.split("   ");
+        left.push(Number(leftRight[0]));
+        right.push(Number(leftRight[1]));
+    }
+
+    const numSort = (a: number, b: number) => a - b;
+    left.sort(numSort);
+    right.sort(numSort);
+
+    let sum = 0;
+    for (let index = 0; index < left.length; index++) {
+        const leftValue = left[index];
+        const rightValue = right[index];
+        
+        sum += Math.abs(leftValue - rightValue);
+    }
+
+    console.log(left.length, right.length)
+
+    return `${sum}`;
 };
 
