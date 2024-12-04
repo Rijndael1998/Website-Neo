16cdb086027ede4f769053c32d87a16ec5d5419e
Rijndael <rijndael@baldy.ga>
2024-12-04 15:43:39 +0000

    passes basic test

--- src/pages/api/advent_of_code/4.ts
+++ src/pages/api/advent_of_code/4.ts
@@ -4,35 +4,91 @@ const isSearch = (row: Array<string>, search: string) => {
     return row.reduce<string>((prev, curr) => prev + curr, "") == search ? 1 : 0;
 }
 
-const search_around = (grid: Array<Array<string>>, x: number, y: number, find: string) => {
-    const leftLimit = x - find.length + 1 >= 0;
-    const rightLimit = x + find.length <= grid[y].length;
-    const bottomLimit = y - find.length + 1 >= 0;
-    const topLimit = y + find.length <= grid.length;
+enum Direction {
+    UP,
+    UP_RIGHT,
+    RIGHT,
+    BOTTOM_RIGHT,
+    BOTTOM,
+    BOTTOM_LEFT,
+    LEFT,
+    UP_LEFT,
+};
 
-    let instances = 0;
+const ALL_DIRECTIONS = [
+    Direction.RIGHT,
+    Direction.BOTTOM_RIGHT,
+    Direction.BOTTOM,
+    Direction.BOTTOM_LEFT,
+    Direction.LEFT,
+    Direction.UP_LEFT,
+    Direction.UP,
+    Direction.UP_RIGHT,
+];
 
-    if(leftLimit) {
-        const res = grid[y].slice(x - find.length + 1, x + 1);
-        instances += isSearch(res.toReversed(), find);
+const check_coords = (grid: Array<Array<string>>, x: number, y: number) => {
+    return y >= grid.length ||
+        y < 0 ||
+        x >= grid[y].length ||
+        x < 0
+}
+
+const search_direction = (grid: Array<Array<string>>, x: number, y: number, direction: Direction, find: Array<string>) => {
+    // exit conditions
+    // no more to find
+    if (find.length == 0)
+        return 1; // found the end
+
+    // invalid coords
+    if (check_coords(grid, x, y))
+        return 0;
+
+    // make new mutable list
+    const newFind = [...find];
+    const searchChar = newFind.shift();
+
+    console.log(direction, newFind);
+
+    // wrong character
+    if (grid[y][x] !== searchChar)
+        return 0;
+
+    switch (direction) {
+        case Direction.UP:
+            return search_direction(grid, x, y + 1, direction, newFind);
+
+        case Direction.UP_RIGHT:
+            return search_direction(grid, x + 1, y + 1, direction, newFind);
+
+        case Direction.RIGHT:
+            return search_direction(grid, x + 1, y, direction, newFind);
+
+        case Direction.BOTTOM_RIGHT:
+            return search_direction(grid, x + 1, y - 1, direction, newFind);
+
+        case Direction.BOTTOM:
+            return search_direction(grid, x, y - 1, direction, newFind);
+
+        case Direction.BOTTOM_LEFT:
+            return search_direction(grid, x - 1, y - 1, direction, newFind);
+
+        case Direction.LEFT:
+            return search_direction(grid, x - 1, y, direction, newFind);
+
+        case Direction.UP_LEFT:
+            return search_direction(grid, x - 1, y + 1, direction, newFind);
+
+        default:
+            return 0;
     }
+}
 
-    if(rightLimit) {
-        const res = grid[y].slice(x, x + find.length);
-        instances += isSearch(res, find);
-    }
-
-    if(bottomLimit) {
-        const res = grid.slice(y - find.length + 1, y + 1).map(v => v[x]);
-        instances += isSearch(res.toReversed(), find);
-    }
-
-    if(topLimit) {
-        const res = grid.slice(y, y + find.length).map(v => v[x]);
-        instances += isSearch(res, find)
-    }
-
-    return instances;
+const search_around = (grid: Array<Array<string>>, x: number, y: number, find: Array<string>) => {
+    return ALL_DIRECTIONS.reduce<number>(
+        (instances, direction) =>
+            instances + search_direction(grid, x, y, direction, find),
+        0
+    );
 }
 
 export const solution_4: AdventOfCodeSolutionFunction = (input) => {
@@ -41,11 +97,13 @@ export const solution_4: AdventOfCodeSolutionFunction = (input) => {
     let part_1 = 0;
     let part_2 = 0;
 
+    const find = "XMAS".split("");
+
     for (let y = 0; y < grid.length; y++) {
         const row = grid[y];
         for (let x = 0; x < row.length; x++) {
             console.log(x, y);
-            part_1 += search_around(grid, x, y, "XMAS");
+            part_1 += search_around(grid, x, y, find);
         }
     }
 
