9944a8d8f303628dec32c7d775c36569fb1d86c9
Rijndael <rijndael@baldy.ga>
2024-12-04 11:16:42 +0000

    added route and page

---
+++ src/pages/api/advent_of_code/4.ts
@@ -0,0 +1,10 @@
+import { AdventOfCodeSolutionFunction } from "./solutions";
+
+export const solution_4: AdventOfCodeSolutionFunction = (input) => {
+
+
+    return {
+        part_1: "",
+        part_2: "",
+    }
+}
