a1ce467e88edd3b2bf406e898b584da4369f17cc
Rijndael <rijndael@baldy.ga>
2024-12-04 14:37:34 +0000

    cardinal directions work but I don't like this solution

--- src/pages/api/advent_of_code/4.ts
+++ src/pages/api/advent_of_code/4.ts
@@ -6,16 +6,31 @@ const isSearch = (row: Array<string>, search: string) => {
 
 const search_around = (grid: Array<Array<string>>, x: number, y: number, find: string) => {
     const leftLimit = x - find.length + 1 >= 0;
-    const rightLimit = x + find.length - 1 < grid[y].length - 1;
+    const rightLimit = x + find.length <= grid[y].length;
     const bottomLimit = y - find.length + 1 >= 0;
-    const topLimit = y + find.length - 1 < grid.length - 1;
-
-    if(leftLimit) {
-        const res = grid[y].slice(x - find.length, x);
-        console.log(res);
-    }
+    const topLimit = y + find.length <= grid.length;
 
     let instances = 0;
+
+    if(leftLimit) {
+        const res = grid[y].slice(x - find.length + 1, x + 1);
+        instances += isSearch(res.toReversed(), find);
+    }
+
+    if(rightLimit) {
+        const res = grid[y].slice(x, x + find.length);
+        instances += isSearch(res, find);
+    }
+
+    if(bottomLimit) {
+        const res = grid.slice(y - find.length + 1, y + 1).map(v => v[x]);
+        instances += isSearch(res.toReversed(), find);
+    }
+
+    if(topLimit) {
+        const res = grid.slice(y, y + find.length).map(v => v[x]);
+        instances += isSearch(res, find)
+    }
 
     return instances;
 }
