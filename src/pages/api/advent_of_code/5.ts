f88c0792e159a8428184c5d29ce7912d94f306f7
Rijndael <rijndael@baldy.ga>
2024-12-05 09:11:49 +0000

    added 5th input

---
+++ src/pages/api/advent_of_code/5.ts
@@ -0,0 +1,14 @@
+import { AdventOfCodeSolutionFunction } from "./solutions";
+
+export const solution_5: AdventOfCodeSolutionFunction = (input) => {
+    
+
+
+
+    const res = "Test: " + input;
+    return {
+        part_1: res,
+        part_2: res,
+    }
+}
+
--- src/pages/api/advent_of_code/solutions.ts
+++ src/pages/api/advent_of_code/solutions.ts
@@ -3,6 +3,7 @@ import { solution_1 } from "./1";
 import { solution_2 } from "./2";
 import { solution_3 } from "./3";
 import { solution_4 } from "./4";
+import { solution_5 } from "./5";
 
 export type AdventOfCodeSolution = {
     part_1: string | number,
@@ -18,4 +19,5 @@ export const solutions: {[number: number]: AdventOfCodeSolutionFunction} = {
     2: solution_2,
     3: solution_3,
     4: solution_4,
+    5: solution_5,
 };
