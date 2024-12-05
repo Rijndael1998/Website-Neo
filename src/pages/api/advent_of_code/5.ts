ec0088dbf1be6ca44e23dd8a9b64519ef5813f87
Rijndael <rijndael@baldy.ga>
2024-12-05 12:38:28 +0000

    refactored a little

--- src/pages/api/advent_of_code/5.ts
+++ src/pages/api/advent_of_code/5.ts
@@ -1,11 +1,44 @@
 import { AdventOfCodeSolutionFunction } from "./solutions";
 
+type RulesType = Map<number, Array<number>>;
+
 const ReduceMiddleNumbers = (p: number, v: Array<number>) => p + v[(v.length - 1) / 2];
+
+const CheckPage = (pages: Array<number>, rules: RulesType) => {
+    for (let index = 0; index < pages.length; index++) {
+        const page = pages[index]; // [97,61,53,29,13] => 97
+        const elementRules = rules.get(page);
+
+        // there are no rules for this number
+        if (elementRules === undefined)
+            continue;
+
+        for (let ruleIndex = 0; ruleIndex < elementRules.length; ruleIndex++) {
+            const rule = elementRules[ruleIndex];
+            const rulePos = pages.indexOf(rule);
+
+            // the number specified in the rule doesn't exist in our page
+            if (rulePos == -1)
+                continue;
+
+            // the number came before our current index, meaning it's bad.
+            if (index > rulePos) 
+                return false;
+        }
+    }
+
+    return true;
+}
+
+
+const Reposition = (page: Array<number>, rules: RulesType) => {
+
+}
 
 export const solution_5: AdventOfCodeSolutionFunction = (input) => {
     const [rules_input, content_input] = input.split("\n\n").map(v => v.trim());
 
-    const rules = new Map<number, Array<number>>();
+    const rules: RulesType = new Map();
 
     rules_input.split("\n").map(v => v.split("|").map(v => Number(v))).forEach((rule) => {
         const [k, v] = rule;
@@ -15,38 +48,23 @@ export const solution_5: AdventOfCodeSolutionFunction = (input) => {
             rules.set(k, [v]);
     });
 
-
     const correctArray: Array<Array<number>> = [];
     const incorrectArray: Array<Array<number>> = [];
 
     content_input.split("\n").map(v => v.split(",").map(v => Number(v))).forEach(pages => {
-        for (let index = 0; index < pages.length; index++) {
-            const page = pages[index]; // [97,61,53,29,13] => 97
-            const elementRules = rules.get(page);
-
-            // there are no rules for this number
-            if (elementRules === undefined)
-                continue;
-
-            for (let ruleIndex = 0; ruleIndex < elementRules.length; ruleIndex++) {
-                const rule = elementRules[ruleIndex];
-                const rulePos = pages.indexOf(rule);
-                // the number specified in the rule doesn't exist in our page
-                if (rulePos == -1)
-                    continue;
-
-                // the number came before our current index, meaning it's bad.
-                if (index > rulePos) {
-                    incorrectArray.push(pages);
-                    return;
-                }
-            }
-        }
-
-        correctArray.push(pages);
+        if(CheckPage(pages, rules))
+            correctArray.push(pages);
+        else
+            incorrectArray.push(pages);
     });
-    
+
     const part_1 = correctArray.reduce<number>(ReduceMiddleNumbers, 0);
+
+    incorrectArray.map((v) => {
+        const res = Reposition([...v], rules);
+        console.log(v, res);
+        return res;
+    });
 
     return {
         part_1,
