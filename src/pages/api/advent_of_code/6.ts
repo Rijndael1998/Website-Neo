b0b3f7d57452b75654ba73d4cad3117ef94565da
Rijndael <rijndael@baldy.ga>
2024-12-06 09:44:04 +0000

    added day 6

---
+++ src/pages/api/advent_of_code/6.ts
@@ -0,0 +1,11 @@
+import { AdventOfCodeSolutionFunction } from "./solutions";
+
+
+export const solution_6: AdventOfCodeSolutionFunction = (input) => {
+    
+
+    return {
+        part_1: "",
+        part_2: "",
+    }
+}
