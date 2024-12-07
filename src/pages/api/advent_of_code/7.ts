8707c3d0f9166b240a38c451ae92d876d377d048
Rijndael <rijndael@baldy.ga>
2024-12-07 10:29:38 +0000

    formatted numbers nicely

--- src/pages/api/advent_of_code/7.ts
+++ src/pages/api/advent_of_code/7.ts
@@ -2,6 +2,17 @@ import { AdventOfCodeSolutionFunction } from "./solutions";
 
 export const solution_7: AdventOfCodeSolutionFunction = (input) => {
     const res = "Test: " + input;
+
+    const numbers =
+        input.split("\n")
+            .map(
+                (v) => v.trim()
+                    .split(":")
+                    .map(v => v.trim().split(" ").map(v => Number(v)))
+            );
+    
+    console.log(numbers);
+
     return {
         part_1: res,
         part_2: res,
