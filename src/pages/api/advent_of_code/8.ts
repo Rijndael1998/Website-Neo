9cbdc91faa6f4d9dbc1207a59becae950e4fbc0d
Rijndael <rijndael@baldy.ga>
2024-12-08 13:58:51 +0000

    fancy new code

--- src/pages/api/advent_of_code/8.ts
+++ src/pages/api/advent_of_code/8.ts
@@ -1,10 +1,21 @@
 import { AdventOfCodeSolutionFunction } from "./solutions";
+import { MakeEmpty2DArray, MakeEmptyArray, makeGridFromMultilineString } from "./utils/utils";
 
 export const solution_8: AdventOfCodeSolutionFunction = (input) => {
-    const res = "Test: " + input;
+    let part_1 = 0;
+    let part_2 = 0;
+
+    const grid = makeGridFromMultilineString(input);
+    const nodes = new Map<string, Array<[x: number, y: number]>>();
+    const antinodeLocations = MakeEmpty2DArray(grid.length, grid[0].length);
+
+    
+
+    console.log(antinodeLocations);
+
     return {
-        part_1: res,
-        part_2: res,
+        part_1,
+        part_2,
     }
 }
 
--- src/pages/api/advent_of_code/utils/utils.ts
+++ src/pages/api/advent_of_code/utils/utils.ts
@@ -5,3 +5,14 @@ export const Duplicate2DArray = <T>(array: Array<Array<T>>) => {
     return [...array.map((item) => [...item])];
 }
 
+export const MakeEmptyGenericArray = <T>(length: number, fn: (index: number) => T) => {
+    const newArray = [];
+    let i = 0;
+    while (i++ < length)
+        newArray.push(fn(i));
+
+    return newArray;
+}
+
+export const MakeEmptyArray = (length: number) => MakeEmptyGenericArray(length, () => 0);
+export const MakeEmpty2DArray = (x: number, y: number) => MakeEmptyArray(y).map(() => MakeEmptyArray(x));
