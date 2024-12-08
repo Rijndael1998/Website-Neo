5589710488fa5c2fc9a62d668790d125da2683e4
Rijndael <rijndael@baldy.ga>
2024-12-08 14:04:39 +0000

    finds the nodes

--- src/pages/api/advent_of_code/8.ts
+++ src/pages/api/advent_of_code/8.ts
@@ -7,11 +7,25 @@ export const solution_8: AdventOfCodeSolutionFunction = (input) => {
 
     const grid = makeGridFromMultilineString(input);
     const nodes = new Map<string, Array<[x: number, y: number]>>();
+    const nodeKinds = [];
     const antinodeLocations = MakeEmpty2DArray(grid.length, grid[0].length);
 
-    
+    // find all the nodes
+    grid.forEach((row, y) => row.forEach((item, x) => {
+        if(item == ".")
+            return;
 
-    console.log(antinodeLocations);
+        if(nodes.has(item))
+            nodes.get(item)!.push([x, y]);
+
+        else {
+            nodes.set(item, [[x, y]]);
+            nodeKinds.push(item);
+        }
+
+    }));
+
+    console.log(nodes);
 
     return {
         part_1,
