135cc19ac826fbe261812e8b4e7869f2992b7ba9
Rijndael <rijndael@baldy.ga>
2024-12-11 20:47:24 +0000

    maybe implemented rules

--- src/pages/api/advent_of_code/11.ts
+++ src/pages/api/advent_of_code/11.ts
@@ -7,7 +7,33 @@ class Stone {
         this.val = val;
     }
 
-    
+    length() {
+        return Math.floor(Math.log10(this.val)) + 1;
+    }
+
+    hasEvenDigits() {
+        return this.length() % 2 == 0;
+    }
+
+    split() {
+        const valSplitPoint = 10 ** (this.length() / 2);
+        const valRight = this.val % valSplitPoint;
+        const valLeft = Math.floor((this.val - valRight) / valSplitPoint);
+        return [new Stone(valLeft), new Stone(valRight)];
+    }
+
+    blink(): Array<Stone> {
+        if (this.val == 0) {
+            this.val = 1;
+            return [this];
+        }
+
+        if(this.hasEvenDigits())
+            return this.split();
+
+        this.val = 2024;
+        return [this];
+    }
 }
 
 export const solution_11: AdventOfCodeSolutionFunction = (input) => {
