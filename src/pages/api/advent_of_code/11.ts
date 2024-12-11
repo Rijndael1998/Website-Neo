4c2c7927086ea8836367610929e5c375d6276b9b
Rijndael <rijndael@baldy.ga>
2024-12-11 22:11:39 +0000

    reduced number of inputs

--- src/pages/api/advent_of_code/11.ts
+++ src/pages/api/advent_of_code/11.ts
@@ -20,29 +20,31 @@ const log = (input: Array<number>) =>
     console.log(prettyPrint(input));
 
 
-const calculatePaths = (input: number, depthLimit = 50, depthCount = 0): number => {
-    if(depthCount == depthLimit)
+const calculatePaths = (input: number, ttl = 50): number => {
+    if (ttl == 0)
         return 1;
 
-    if(input == 0)
-        return calculatePaths(1, depthLimit, depthCount + 1);
+    if (input == 0)
+        return calculatePaths(1, ttl - 1);
 
-    if(numHasEvenDigits(input)) {
+    if (numHasEvenDigits(input)) {
         // copied from numSplit as to not create and destruct an object as that causes the GC to get cranky
         const valSplitPoint = 10 ** (numLength(input) / 2);
         const valRight = input % valSplitPoint;
         const valLeft = Math.floor((input - valRight) / valSplitPoint);
-        return calculatePaths(valRight, depthLimit, depthCount + 1) + calculatePaths(valLeft, depthLimit, depthCount + 1);
+        return calculatePaths(valRight, ttl - 1) + calculatePaths(valLeft, ttl - 1);
     }
-    
-    return calculatePaths(input * 2024, depthLimit, depthCount + 1);
+
+    return calculatePaths(input * 2024, ttl - 1);
 }
 
 export const solution_11: AdventOfCodeSolutionFunction = (input) => {
     const stones = input.trim().split(" ").map((v) => Number(v));
 
     return {
+        // 193607
         part_1: stones.reduce<number>((prev, curr) => prev + calculatePaths(curr, 25), 0),
+        // ???
         part_2: stones.length,
     }
 }
