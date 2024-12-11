73fdec5be36aebe18cb044e3888d4871e066f236
Rijndael <rijndael@baldy.ga>
2024-12-11 21:46:20 +0000

    faster but not fast enough

--- src/pages/api/advent_of_code/11.ts
+++ src/pages/api/advent_of_code/11.ts
@@ -1,9 +1,9 @@
 import { AdventOfCodeSolutionFunction } from "./solutions";
 
-const numLength = (num: number) => 
+const numLength = (num: number) =>
     Math.floor(Math.log10(num)) + 1;
 
-const numHasEvenDigits = (num: number) => 
+const numHasEvenDigits = (num: number) =>
     numLength(num) % 2 == 0;
 
 const numSplit = (num: number) => {
@@ -21,12 +21,12 @@ const log = (input: Array<number>) =>
 
 const blinkInPlace = (stones: Array<number>) => {
     for (let index = 0; index < stones.length; index++) {
-        if(stones[index] == 0) {
+        if (stones[index] == 0) {
             stones[index] = 1;
             continue;
         }
 
-        if(numHasEvenDigits(stones[index])) {
+        if (numHasEvenDigits(stones[index])) {
             stones.splice(index, 1, ...numSplit(stones[index]));
             index++;
             continue;
@@ -38,14 +38,19 @@ const blinkInPlace = (stones: Array<number>) => {
 
 export const solution_11: AdventOfCodeSolutionFunction = (input) => {
     const stones = input.trim().split(" ").map((v) => Number(v));
-    log(stones);
 
-    // blink the stones
-    blinkInPlace(stones);
+    // blink the stones 25 times
+    for (let blink = 0; blink < 25; blink++)
+        blinkInPlace(stones);
 
-    log(stones);
+    const part_1 = stones.length;
 
-    const part_1 = 0;
+    // blink the stones 50 more times
+    for (let blink = 0; blink < 50; blink++) {
+        blinkInPlace(stones);
+        console.log(blink + 25, Date());
+    }
+        
 
     return {
         part_1,
