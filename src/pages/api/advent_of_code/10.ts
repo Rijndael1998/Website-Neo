995a7583846320eeed56d692c0fe117d93259ed1
Rijndael <rijndael@baldy.ga>
2024-12-11 00:55:00 +0000

    gets basic answer correct

--- src/pages/api/advent_of_code/10.ts
+++ src/pages/api/advent_of_code/10.ts
@@ -5,7 +5,6 @@ import { Point } from "./utils/structures/point";
 import { Duplicate2DArray, makeGridFromMultilineString, prettyPrint2d } from "./utils/utils";
 
 export const solution_10: AdventOfCodeSolutionFunction = (input) => {
-    let part_1 = 0;
     let part_2 = 0;
 
     const map: Grid<LinkedPoint> =
@@ -15,14 +14,11 @@ export const solution_10: AdventOfCodeSolutionFunction = (input) => {
 
     map.flat().forEach((v) => v.grid = map); // promise is a promise
 
-    console.log(prettyPrint2d(map.map((v) => v.map(v => v.item))));
+    // console.log(prettyPrint2d(map.map((v) => v.map(v => v.item))));
 
     const startNodes: Array<LinkedPoint> = map.flat().filter(v => v.item == 0);
 
-    console.log(startNodes);
-    // startNodes.forEach(v => v.lookAround());
-
-    console.log(...startNodes.map(v => v.findAllValidPeaks().length));
+    const part_1 = startNodes.map(v => v.findAllValidPeaks().length).reduce((prev, curr) => prev + curr, 0);
 
     return {
         part_1,
