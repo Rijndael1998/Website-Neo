a7d9568e628e896dc39f5441250cbf60371c2135
Rijndael <rijndael@baldy.ga>
2024-12-11 21:28:29 +0000

    removed the stone class

--- src/pages/api/advent_of_code/11.ts
+++ src/pages/api/advent_of_code/11.ts
@@ -1,60 +1,29 @@
 import { AdventOfCodeSolutionFunction } from "./solutions";
 
-class Stone {
-    val: number;
+const numLength = (num: number) => 
+    Math.floor(Math.log10(num)) + 1;
 
-    constructor(val: number) {
-        this.val = val;
-    }
+const numHasEvenDigits = (num: number) => 
+    numLength(num) % 2 == 0;
 
-    length() {
-        return Math.floor(Math.log10(this.val)) + 1;
-    }
-
-    hasEvenDigits() {
-        return this.length() % 2 == 0;
-    }
-
-    split() {
-        const valSplitPoint = 10 ** (this.length() / 2);
-        const valRight = this.val % valSplitPoint;
-        const valLeft = Math.floor((this.val - valRight) / valSplitPoint);
-        return [new Stone(valLeft), new Stone(valRight)];
-    }
-
-    blink(): Array<Stone> {
-        if (this.val == 0) {
-            return [new Stone(1)];
-        }
-
-        if (this.hasEvenDigits())
-            return this.split();
-
-        return [new Stone(this.val * 2024)];
-    }
+const numSplit = (num: number) => {
+    const valSplitPoint = 10 ** (numLength(num) / 2);
+    const valRight = num % valSplitPoint;
+    const valLeft = Math.floor((num - valRight) / valSplitPoint);
+    return [valLeft, valRight];
 }
 
-const prettyPrint = (input: Array<Stone>) =>
-    input.reduce<string>((prev, curr) => `${prev} ${curr.val}`, "");
+const prettyPrint = (input: Array<number>) =>
+    input.reduce<string>((prev, curr) => `${prev} ${curr}`, "");
 
-const log = (input: Array<Stone>) =>
+const log = (input: Array<number>) =>
     console.log(prettyPrint(input));
 
 export const solution_11: AdventOfCodeSolutionFunction = (input) => {
-    let stones = input.trim().split(" ").map((v) => new Stone(Number(v)));
+    const stones = input.trim().split(" ").map((v) => Number(v));
     log(stones);
 
-    // blink 25 times
-    for (let blink = 0; blink < 25; blink++) {
-        stones = stones.flatMap(stone => stone.blink());
-    }
-
-    const part_1 = stones.length; // 193607
-
-    // blink 50 times 
-    for (let blink = 0; blink < 50; blink++) {
-        stones = stones.flatMap(stone => stone.blink());
-    }
+    const part_1 = 0;
 
     return {
         part_1,
