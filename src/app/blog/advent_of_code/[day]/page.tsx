923afbb4c99a8642ce6b41e31472ea6c87c624c0
Rijndael <rijndael@baldy.ga>
2024-12-11 13:08:36 +0000

    added a not found element

--- src/app/blog/advent_of_code/[day]/page.tsx
+++ src/app/blog/advent_of_code/[day]/page.tsx
@@ -1,6 +1,8 @@
 import { Container, Stack, Typography } from "@mui/material";
 import Solution from "./solution";
 import DisplayNav from "./displayNav";
+import { DAY } from "../day";
+import { ifTruthyElse } from "@/components/reactUtils";
 
 export type AdventOfCodeDaysProps = {
     params: Promise<{ day: string }>,
@@ -8,13 +10,23 @@ export type AdventOfCodeDaysProps = {
 
 export default async function AdventOfCodeDays({ params }: AdventOfCodeDaysProps) {
     const day = Number((await params).day);
+    const enable = day <= DAY && day > 0;
+
+    const SolutionElement = <>
+        <Typography variant="body1">{`Fill in your puzzle's input`}</Typography>
+        <Solution day={day} />
+    </>
+
+    const NotFoundElement = <Typography>
+        The solution to this problem is currently in construction. A+ for effort, though 😅.
+    </Typography>
+
     return <Container maxWidth="md">
         <Stack gap={1}>
             <Typography variant="h1">{`Day ${day} solution`}</Typography>
-            <DisplayNav day={day}/>
+            <DisplayNav day={day} />
 
-            <Typography variant="body1">{`Fill in your puzzle's input`}</Typography>
-            <Solution day={day} />
+            {ifTruthyElse(enable, SolutionElement, NotFoundElement)}
         </Stack>
     </Container>
 }
