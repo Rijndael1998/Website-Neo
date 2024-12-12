e1ff7b6b2478491a6b68a0d0583a0cbdbd04e4b0
Rijndael <rijndael@baldy.ga>
2024-12-12 23:10:01 +0000

    Made points

--- src/pages/api/advent_of_code/12.ts
+++ src/pages/api/advent_of_code/12.ts
@@ -1,8 +1,19 @@
 import { AdventOfCodeSolutionFunction } from "./solutions";
+import { Point } from "./utils/structures/point";
+import { makeGridFromMultilineString } from "./utils/utils";
+
+class Plot {
+    points: Array<Point<string>> = [];
+    kind: string;
+    
+    constructor(kind: string) {
+        this.kind = kind;
+    }
+}
 
 export const solution_12: AdventOfCodeSolutionFunction = (input) => {
-    
-
+    const grid = makeGridFromMultilineString(input).map((v, y) => v.map((v, x) => new Point<string>(x, y, v)));
+    console.log(grid);
     
     const res = "Test: " + input;
     return {
