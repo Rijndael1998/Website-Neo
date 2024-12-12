3756d9affb4f289c78019a1e9d61e2fe05da143a
Rijndael <rijndael@baldy.ga>
2024-12-12 23:13:01 +0000

    Made PlotPoints

--- src/pages/api/advent_of_code/12.ts
+++ src/pages/api/advent_of_code/12.ts
@@ -1,18 +1,25 @@
 import { AdventOfCodeSolutionFunction } from "./solutions";
+import { LinkedPoint } from "./utils/structures/linkedPoint";
 import { Point } from "./utils/structures/point";
 import { makeGridFromMultilineString } from "./utils/utils";
 
 class Plot {
     points: Array<Point<string>> = [];
     kind: string;
-    
+
     constructor(kind: string) {
         this.kind = kind;
     }
 }
 
+class PlotPoint extends LinkedPoint<string, PlotPoint> {
+
+}
+
 export const solution_12: AdventOfCodeSolutionFunction = (input) => {
-    const grid = makeGridFromMultilineString(input).map((v, y) => v.map((v, x) => new Point<string>(x, y, v)));
+    const grid = makeGridFromMultilineString(input).map((v, y) => v.map((v, x) => new PlotPoint(x, y, v, undefined!)));
+    grid.flat().map(v => v.grid = grid);
+
     console.log(grid);
     
     const res = "Test: " + input;
