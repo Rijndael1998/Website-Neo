4cf6a1e994f56cb8589aa4ae27058d218597fe3b
Rijndael <rijndael@baldy.ga>
2024-12-13 00:25:59 +0000

    pretty print

--- src/pages/api/advent_of_code/12.ts
+++ src/pages/api/advent_of_code/12.ts
@@ -1,6 +1,7 @@
 import { AdventOfCodeSolutionFunction } from "./solutions";
 import { LinkedPoint } from "./utils/structures/linkedPoint";
-import { makeGridFromMultilineString } from "./utils/utils";
+import { Vector } from "./utils/structures/vector";
+import { MakeEmpty2DArray, makeGridFromMultilineString } from "./utils/utils";
 
 class Plot {
     points: Array<PlotPoint> = [];
@@ -29,6 +30,10 @@ class Plot {
     getPerimeter() {
         return this.points.reduce<number>((prev, curr) => prev + curr.getPerimeter(), 0);
     }
+
+    getPointsOnEdges() {
+        return this.points.filter(v => v.getPerimeter() > 0);
+    }
 }
 
 class PlotPoint extends LinkedPoint<string, PlotPoint> { 
@@ -37,6 +42,27 @@ class PlotPoint extends LinkedPoint<string, PlotPoint> {
     getPerimeter() {
         return 4 - super.lookAround().filter((v) => v.item == this.item).length;
     }
+}
+
+const prettyPrint = (points: Array<Vector>) => {
+    const minX = points.map(v => v.x).reduce((p, c) => Math.min(p, c), Number.POSITIVE_INFINITY);
+    const minY = points.map(v => v.y).reduce((p, c) => Math.min(p, c), Number.POSITIVE_INFINITY);
+    const maxX = points.map(v => v.x).reduce((p, c) => Math.max(p, c), Number.NEGATIVE_INFINITY);
+    const maxY = points.map(v => v.y).reduce((p, c) => Math.max(p, c), Number.NEGATIVE_INFINITY);
+    const xL = maxX - minX;
+    const yL = maxY - minY;
+
+    console.log(minX, minY, minY, maxY, xL, yL);
+
+    const grid = MakeEmpty2DArray(xL + 1, yL + 1).map(r => r.map(_ => " "));
+
+    console.log(grid);
+
+    points.forEach((v) => {
+        grid[v.y - minY][v.x - minX] = "*";
+    });
+
+    return grid.reduce((prev, curr) => `${prev}\n${curr.reduce((prev, curr) => `${prev}${curr}`, "")}`, "");
 }
 
 export const solution_12: AdventOfCodeSolutionFunction = (input) => {
@@ -54,6 +80,8 @@ export const solution_12: AdventOfCodeSolutionFunction = (input) => {
         plot.addPoint(v);
         plots.push(plot);
     });
+
+    console.log(prettyPrint(plots[0].getPointsOnEdges().map(v => v.pos)));
 
     const part_1 = plots.reduce<number>((prev, curr) => prev + curr.getArea() * curr.getPerimeter(), 0);
 
