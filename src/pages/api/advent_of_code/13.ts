1051de204b023da192110ee10d86533af52f0adc
Rijndael <rijndael@baldy.ga>
2024-12-31 23:27:11 +0000

    refactor

--- src/pages/api/advent_of_code/13.ts
+++ src/pages/api/advent_of_code/13.ts
@@ -14,12 +14,6 @@ class Machine {
     constructor(public buttonA: Vector, public buttonB: Vector, public prize: Vector) { }
 }
 
-type part1Solution = false | {
-    aCount: number,
-    bCount: number,
-    cost: number,
-}
-
 const MAX_PRESSES = 100;
 const A_COST = 3;
 const B_COST = 1;
@@ -29,8 +23,10 @@ export const solution_13: AdventOfCodeSolutionFunction = (input) => {
     const machines = input.split("\n\n").map((v) => {
         const [a, b, target] = v.split("\n").map(getXY);
         return new Machine(a, b, target);
-    }).map(machine => {
-        let cheapestSolution: part1Solution = false;
+    });
+
+    const part_1 = machines.map(machine => {
+        let cheapestSolution: number = Number.POSITIVE_INFINITY;
 
         aCountLoop: for (let aCount = 0; aCount < MAX_PRESSES; aCount++) {
             for (let bCount = 0; bCount < MAX_PRESSES; bCount++) {
@@ -39,26 +35,25 @@ export const solution_13: AdventOfCodeSolutionFunction = (input) => {
                 const distance = aDistance.add(bDistance);
 
                 const difference = machine.prize.sub(distance);
-                if(difference.x < 0 || difference.y < 0)
+                if (difference.x < 0 || difference.y < 0)
                     continue aCountLoop;
 
                 if (distance.compare(machine.prize)) {
                     // found solution
                     const cost = aCount * A_COST + bCount * B_COST;
-                    if (cheapestSolution === false || cheapestSolution.cost > cost)
-                        cheapestSolution = { aCount, bCount, cost };
+                    cheapestSolution = Math.min(cheapestSolution, cost);
                 }
             }
         }
 
         return cheapestSolution;
-    });
+    }).reduce((prev, curr) => prev + (curr == Number.POSITIVE_INFINITY ? 0 : curr));
 
 
 
     return {
         // 37901
-        part_1: machines.reduce<number>((prev, curr) => prev + (curr == false ? 0 : curr.cost), 0),
+        part_1,
         part_2: ""
     }
 }
