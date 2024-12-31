296a83ab4c947d360cf4e8c04e57150a473a25d0
Rijndael <rijndael@baldy.ga>
2024-12-31 22:37:47 +0000

    first part correct

--- src/pages/api/advent_of_code/13.ts
+++ src/pages/api/advent_of_code/13.ts
@@ -14,36 +14,13 @@ class Machine {
     constructor(public buttonA: Vector, public buttonB: Vector, public prize: Vector) { }
 }
 
-type findAnswerReturn = false | {
+type part1Solution = false | {
     aCount: number,
     bCount: number,
+    cost: number,
 }
 
-//                               aButton, bButton
-type findAnswerMemoType = Memo2D<number, number, findAnswerReturn>;
-
-const findAnswer = (aCount: number, bCount: number, machine: Machine, memo: findAnswerMemoType): findAnswerReturn => {
-    // speedup from memo
-    const memoRes = memo.getResult(aCount, bCount);
-    if(memoRes !== undefined)
-        return memoRes;
-
-    if (aCount + bCount > 100)
-        return memo.setResult(aCount, bCount, false);
-
-    if (machine.buttonA.scale(aCount).add(machine.buttonB.scale(bCount)).compare(machine.prize))
-        return memo.setResult(aCount, bCount, { aCount, bCount });
-
-    const resB = findAnswer(aCount, bCount + 1, machine, memo);
-
-    if (resB === false) {
-        const resA = findAnswer(aCount + 1, bCount, machine, memo);
-        return memo.setResult(aCount, bCount, resA);
-    }
-
-    return memo.setResult(aCount, bCount, resB);
-}
-
+const MAX_PRESSES = 100;
 const A_COST = 3;
 const B_COST = 1;
 
@@ -51,14 +28,31 @@ export const solution_13: AdventOfCodeSolutionFunction = (input) => {
     const machines = input.split("\n\n").map((v) => {
         const [a, b, target] = v.split("\n").map(getXY);
         return new Machine(a, b, target);
-    }).map(m => {
-        return findAnswer(0, 0, m, new Memo2D());
+    }).map(machine => {
+        let cheapestSolution: part1Solution = false;
+
+        for (let aCount = 0; aCount < MAX_PRESSES; aCount++) {
+            for (let bCount = 0; bCount < MAX_PRESSES; bCount++) {
+                const aDistance = machine.buttonA.scale(aCount);
+                const bDistance = machine.buttonB.scale(bCount);
+                const distance = aDistance.add(bDistance);
+
+                if (distance.compare(machine.prize)) {
+                    // found solution
+                    const cost = aCount * A_COST + bCount * B_COST;
+                    if (cheapestSolution === false || cheapestSolution.cost > cost)
+                        cheapestSolution = { aCount, bCount, cost };
+                }
+            }
+        }
+
+        return cheapestSolution;
     });
 
-    console.log(machines);
+
 
     return {
-        part_1: "",
+        part_1: machines.reduce<number>((prev, curr) => prev + (curr == false ? 0 : curr.cost), 0),
         part_2: ""
     }
 }
