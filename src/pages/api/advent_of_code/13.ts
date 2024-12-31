d8eed1075edf8bc4b06b3a3773f3904f69c2bd97
Rijndael <rijndael@baldy.ga>
2024-12-31 20:58:55 +0000

    parses the input

--- src/pages/api/advent_of_code/13.ts
+++ src/pages/api/advent_of_code/13.ts
@@ -1,10 +1,25 @@
 import { AdventOfCodeSolutionFunction } from "./solutions";
+import { Vector } from "./utils/structures/vector";
+
+const find = /X.(\d+), Y.(\d+)/;
+const getXY = (input: string) => {
+    const res = input.match(find);
+    const x = Number.parseFloat(res![1]);
+    const y = Number.parseFloat(res![2]);
+    return new Vector(x, y);
+}
 
 export const solution_13: AdventOfCodeSolutionFunction = (input) => {
-    const res = "Test: " + input;
+    const machines = input.split("\n\n").map((v) => {
+        const [buttonA, buttonB, Prize] = v.split("\n").map(getXY)
+        return { buttonA, buttonB, Prize };
+    });
+
+    console.log(machines);
+
     return {
-        part_1: res,
-        part_2: res,
+        part_1: "",
+        part_2: ""
     }
 }
 
