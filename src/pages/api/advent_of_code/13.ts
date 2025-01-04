2c2e7488f82ad8aabbbb505749f193bfe1a0b06f
Rijndael <rijndael@baldy.ga>
2025-01-04 20:49:12 +0000

    bin walk isn't working

--- src/pages/api/advent_of_code/13.ts
+++ src/pages/api/advent_of_code/13.ts
@@ -27,26 +27,27 @@ class Machine {
 type solution = {
     aPresses: number,
     bPresses: number,
+    cost: number,
 } | undefined;
 
-const solve = (machine: Machine, pressLimit?: number, bUpper?: number, bLower: number = 0, ): solution => {
+const solve = (machine: Machine, pressLimit?: number, bUpper?: number, bLower: number = 0,): solution => {
     const target = machine.prize;
 
     // initial values
     pressLimit ??= Number.POSITIVE_INFINITY;
-    bUpper ??= target.div(machine.buttonB).smaller();
+    bUpper ??= Math.floor(target.div(machine.buttonB).smaller());
 
     bUpper = Math.min(pressLimit, bUpper);
 
-    if(bUpper < bLower)
+    if (bUpper < bLower)
         return;
 
-    const midpoint = Math.floor((bUpper - bLower) / 2); // midpoint
+    const midpoint = Math.floor((bUpper + bLower) / 2); // midpoint
 
-    // check the upmost bValue
-    const bPressDistance = machine.buttonB.scale(bUpper);
+    // check the midpoint bValue
+    const bPressDistance = machine.buttonB.scale(midpoint);
     const deltaDistance = machine.prize.sub(bPressDistance);
-    const aPresses = Math.min(pressLimit, deltaDistance.div(machine.buttonA).smaller());
+    const aPresses = Math.floor(Math.min(pressLimit, deltaDistance.div(machine.buttonA).smaller()));
     const countedAPresses = machine.buttonA.scale(aPresses);
     const distanceNotCovered = deltaDistance.sub(countedAPresses);
     const foundSolution = distanceNotCovered.x + distanceNotCovered.y == 0;
@@ -56,17 +57,21 @@ const solve = (machine: Machine, pressLimit?: number, bUpper?: number, bLower: n
     console.log(bPressDistance, deltaDistance)
     console.log(aPresses, countedAPresses, distanceNotCovered, foundSolution);
 
-    let bestSolution: solution;
+    let bestSolution = !foundSolution ? undefined : {
+        aPresses,
+        bPresses: midpoint,
+        cost: aPresses * A_COST + bUpper * B_COST,
+    };
 
-    // exit conditions
-    if(foundSolution)
-        bestSolution = {
-            aPresses,
-            bPresses: midpoint,
-        };
+    const solUpper = solve(machine, pressLimit, bUpper, midpoint + 1);
+    if(solUpper)
+        return solUpper;
 
-    
+    if(bestSolution)
+        return bestSolution;
 
+    const solLower = solve(machine, pressLimit, bLower - 1, midpoint);
+    return solLower;
 }
 
 const reduceSum = (prev: number, curr: number) => prev + (curr == Number.POSITIVE_INFINITY ? 0 : curr);
@@ -78,7 +83,7 @@ export const solution_13: AdventOfCodeSolutionFunction = (input) => {
         return machine;
     });
 
-    const part_1 = machines.map(m => solve(m, MAX_PRESSES));
+    const part_1 = machines.map(m => solve(m, MAX_PRESSES)).map(console.log);
     const part_2 = 0; // machines.map(m => solve(m.toCalibrated(), Number.POSITIVE_INFINITY)).reduce(reduceSum);
 
     return {
