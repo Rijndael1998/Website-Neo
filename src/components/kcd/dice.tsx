cd7c8b527912e33a543142a204a3dae13087b8a4
Rijndael <rijndael@baldy.ga>
2025-03-13 23:16:37 +0000

    removed perspective, added side logic

--- src/components/kcd/dice.tsx
+++ src/components/kcd/dice.tsx
@@ -1,16 +1,31 @@
+import { HTMLAttributes } from "react";
 import diceStyle from "./dice.module.scss";
 
 
-export type DiceTypeValidSides = 1 | 2 | 3 | 4 | 5 | 6;
+export type DiceTypeValidSides = 1 | 2 | 3 | 4 | 5 | 6 | number;
 
 export type DiceType = {
     showSide: DiceTypeValidSides,
 }
 
+const degrees: Array<[number, number]> =
+[
+    [0, 0],
+    [180, 0],
+    [-90, 0],
+    [90, 0],
+    [0, -90],
+    [0, 90],
+];
+
 export default function Dice({ showSide }: DiceType) {
+    const [degY, degZ] = degrees[showSide - 1];
 
+    const newStyle: HTMLAttributes<HTMLDivElement>["style"] = {
+        transform: `rotateY(${degY}deg) rotateX(${degZ}deg)`,
+    };
 
-    return <div className={diceStyle.diceRoot}>
+    return <div className={diceStyle.diceRoot} style={newStyle}>
         {[1, 2, 3, 4, 5, 6].map(v =>
             <div key={v} className={diceStyle.side}>
                 <div className={diceStyle.face}>
--- src/components/kcd/kcd.tsx
+++ src/components/kcd/kcd.tsx
@@ -1,13 +1,28 @@
 "use client";
 
-import { Stack } from "@mui/material";
+import { Button, Stack } from "@mui/material";
 import Dice, { DiceTypeValidSides } from "./dice";
 import { useState } from "react";
 
 export default function KCD() {
     const [side, setSide] = useState<DiceTypeValidSides>(1);
 
-    return <Stack sx={{height: "100vh"}}>
-        <Dice showSide={side}/>
-    </Stack>
+    const setSideFn = () => {
+        if (side >= 6)
+            return setSide(1);
+
+        setSide(side + 1);
+    };
+
+    return <>
+        <Stack>
+            <Dice showSide={side} />
+            <Dice showSide={side} />
+            <Dice showSide={side} />
+        </Stack>
+
+        <Button onClick={setSideFn}>
+            Add
+        </Button>
+    </>
 }
