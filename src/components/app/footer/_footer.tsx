eb26615cb71aa106c809875dcec3cbd93aff7b84
Rijndael <rijndael@baldy.ga>
2025-07-21 23:09:28 +0100

    aligned text to the right, made it smaller so the block doesn't span the entire screen...

--- src/components/app/footer/_footer.tsx
+++ src/components/app/footer/_footer.tsx
@@ -53,7 +53,7 @@ export default function Footer() {
         <Quotes />
         <div style={{ display: "flex", flexDirection: "column", textAlign: "right", margin: "0", opacity: 0.12 }}>
             <Git />
-            <Link href={"/debug/"} style={{ fontSize: "0.5em", display: "block", width: "unset" }}>
+            <Link href={"/debug/"} style={{ fontSize: "0.5em", display: "block", width: "6em", marginLeft: "auto" }}>
                 <span>debug</span>
             </Link>
         </div>
