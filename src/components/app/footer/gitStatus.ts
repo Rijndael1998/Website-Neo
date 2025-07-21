74d40e52c0ad2153b86280a5fa8f91452aa3a332
Rijndael <rijndael@baldy.ga>
2025-07-21 13:45:45 +0100

    extracted out

---
+++ src/components/app/footer/gitStatus.ts
@@ -0,0 +1,76 @@
+import { executeCommand } from "@/components/serverUtils";
+
+export enum Status {
+    INVALID = "Invalid Git",
+    BEHIND = "Behind",
+    CURRENT = "Current",
+    UNSET = "Unset",
+    DEV = "Dev",
+}
+
+export class GitStatus {
+    lastUpdate!: number;
+    gitHash?: string;
+    gitHashShort?: string;
+    status: Status = Status.UNSET;
+    branchStatus!: [number, number];
+    branch!: string;
+
+    constructor() {
+        this.Update();
+    }
+
+    async Update() {
+        this.lastUpdate = Date.now();
+
+        // get branch name
+        this.branch = (await executeCommand("git branch | grep \*") ?? " * !! unknown !!").replace(" *", "");
+
+        // update git repo
+        await executeCommand("git fetch");
+
+        // hash
+        const hash = await executeCommand("git rev-parse HEAD");
+        this.gitHash = hash ?? "";
+        this.gitHashShort = this.gitHash.slice(0, 7);
+
+        // check if the current branch is in dev
+        if (this.branch != "main")
+            return this.status = Status.DEV;
+
+        // check if current commit is the last
+        const branch = await executeCommand("git rev-list --left-right --count origin/$(git rev-parse --abbrev-ref HEAD)...HEAD");
+        if (hash === null || branch === null)
+            return this.status = Status.INVALID;
+
+        const [behind, ahead] = branch.split(" ").filter(v => v.length > 0).map(v => Number.parseInt(v));
+        this.branchStatus = [behind, ahead];
+
+        if (behind > 0)
+            return this.status = Status.BEHIND;
+
+        return this.status = Status.CURRENT;
+    }
+
+    async ConsiderUpdate() {
+        if (Date.now() - this.lastUpdate > 1000 * 60 * 60) // ms in sec * sec in min * min in hour
+            await this.Update();
+    }
+}
+
+export function GitStatusToColor(status: Status): string {
+    switch(status) {
+        case Status.UNSET:
+        case Status.INVALID:
+            return "#606";
+
+        case Status.CURRENT:
+            return "#0f0";
+
+        case Status.DEV:
+            return "#00f";
+
+        case Status.BEHIND:
+            return "#f00";
+    }
+}
