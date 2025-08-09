e47a3a4959297d73af74006a1bfb9fd8ea40cd29
Rijndael <rijndael@baldy.ga>
2025-08-09 01:29:55 +0100

    removed scss from youtube

--- src/components/external/youtube/_embed.tsx
+++ src/components/external/youtube/_embed.tsx
@@ -1,10 +1,17 @@
-import styles from "./embed.module.scss";
-
 export type YoutubeEmbedProps = {
     videoID: string,
     aspectRatio?: number
-}  
+}
 
-export default function YoutubeEmbed({videoID, aspectRatio}: YoutubeEmbedProps) {
-    return <iframe className={styles.yt} style={{aspectRatio: aspectRatio ?? 560 / 315}} src={`https://www.youtube-nocookie.com/embed/${videoID}`} title="YouTube video player" frameBorder="0" allowFullScreen/>
+export default function YoutubeEmbed({ videoID, aspectRatio }: YoutubeEmbedProps) {
+    return <iframe
+        style={{
+            width: "100%",
+            aspectRatio: aspectRatio ?? 560 / 315,
+        }}
+        src={`https://www.youtube-nocookie.com/embed/${videoID}`}
+        title="YouTube video player"
+        frameBorder="0"
+        allowFullScreen
+    />
 }
