6966f865664a4da84fa0cc9538a784617f598562
Rijndael <rijndael@baldy.ga>
2025-08-29 14:32:59 +0100

    undo iframe change

--- src/components/external/iFrameEmbed.tsx
+++ src/components/external/iFrameEmbed.tsx
@@ -7,15 +7,13 @@ export type iFrameEmbedProps = {
 }
 
 export default function IFrameEmbed({ source, aspectRatio, extraProps }: iFrameEmbedProps) {
-    return <Hide externalLink="asdf">
-        <iframe
-            style={{
-                width: "100%",
-                aspectRatio: aspectRatio ?? 16 / 9,
-            }}
-            src={source}
-            frameBorder="0"
-            {...extraProps}
-        />
-    </Hide>
+    return <iframe
+        style={{
+            width: "100%",
+            aspectRatio: aspectRatio ?? 16 / 9,
+        }}
+        src={source}
+        frameBorder="0"
+        {...extraProps}
+    />
 }
