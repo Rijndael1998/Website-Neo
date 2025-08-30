c51c26ba247a0b0988a9f493bf3a4b8142928fc9
Rijndael <rijndael@baldy.ga>
2025-08-30 23:55:41 +0100

    happy path ok

--- src/components/app/footer/creds/creds.tsx
+++ src/components/app/footer/creds/creds.tsx
@@ -7,8 +7,21 @@ import cpf from "./images/cloud_practitioner_foundational.png";
 import da from "./images/developer_associate.png";
 import sc from "./images/security_specialist.png";
 import { DialogBox } from "@/components/muiWrappers/popup/DialogBox";
+import Link from "next/link";
 
-
+function Body({ award, id }: { award: string, id: string }) {
+    return <>
+        <p style={{marginTop: 0}}>
+            {`I hold the AWS ${award} certification.`}
+        </p>
+        <p>
+            {`Verification is available at `}<Link href={url}>{url}</Link>.
+        </p>
+        <p style={{marginBottom: 0, fontSize: "0.8em"}}>
+            (Credential ID: <span style={{fontFamily: "monospace"}}>{id}</span>).
+        </p>
+    </>
+}
 
 const url = "https://aws.amazon.com/verification";
 export function CredImage({ img, alt, verification }: { img: LocalLazyImageProps["src"], alt: string, verification?: string }) {
@@ -52,7 +65,7 @@ export function CredImage({ img, alt, verification }: { img: LocalLazyImageProps
             handleClose={handleClose}
             title={alt}
             descriptionElementRef={descriptionElementRef}
-            body={"verify verify something something"}
+            body={verification ? <Body award={alt} id={verification} /> : ""}
             closeText={undefined}
             link={url}
             linkText={"Official verification"}
@@ -62,7 +75,7 @@ export function CredImage({ img, alt, verification }: { img: LocalLazyImageProps
 
 export default function Creds() {
     return <Grid2 container>
-        <CredImage img={cpf} alt="Cloud Practitioner Foundational" verification="5b755d2aa5324fb3a0116d762f54f2ef" />
+        <CredImage img={cpf} alt="Certified Cloud Practitioner (Foundational)" verification="5b755d2aa5324fb3a0116d762f54f2ef" />
         <CredImage img={da} alt="Developer Associate" />
         <CredImage img={sc} alt="Security Specialist" />
     </Grid2>
