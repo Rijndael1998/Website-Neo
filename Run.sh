ce65c0408c4d0ebf30d287b595e63a339ef5aac3
Rijndael <rijndael@baldy.ga>
2025-09-29 22:27:46 +0100

    made more verbose

--- Run.sh
+++ Run.sh
@@ -2,20 +2,28 @@
 
 # Move to folder of the script
 cd "$(dirname "${BASH_SOURCE[0]}")"/
+echo "current wd"
+pwd
 
 # Making sure repo is clean
+echo "resetting git" 
 git fetch --all
 git reset --hard origin/main
 git pull
 
 # Reinit submodules
+echo "resetting submodule"
 git submodule init
 git submodule update --recursive --remote
 
 # Install deps
+echo "yarn install"
 yarn install
 
 # build server version
+echo "deleting build and remaking it"
 rm -vr .next
 yarn run build
+
+echo "running server"
 PORT=10000 yarn run start
